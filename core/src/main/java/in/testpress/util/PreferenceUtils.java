02044ea9862318c19dce680ad676a6afe72dfcc8
vignesh-s <vignesh@testpress.in>
2018-10-31 13:08:11 +0530

    Revert using previous course UI used when opening a new course
    - Use TOC UI as default view when first time user opening a course.

--- core/src/main/java/in/testpress/util/PreferenceUtils.java
+++ core/src/main/java/in/testpress/util/PreferenceUtils.java
@@ -3,7 +3,6 @@ package in.testpress.util;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.support.annotation.NonNull;
 
 import static android.content.Context.MODE_PRIVATE;
 import static junit.framework.Assert.assertNotNull;
@@ -11,7 +10,6 @@ import static junit.framework.Assert.assertNotNull;
 public class PreferenceUtils {
 
     private static final String TESTPRESS_SHARED_PREFS = "TESTPRESS_SHARED_PREFS";
-    private static final String LAST_COURSE_UI_USED_IS_TOC = "LAST_COURSE_UI_USED_IS_TOC";
 
     private static SharedPreferences preferences;
     private static SharedPreferences.Editor editor;
@@ -31,14 +29,6 @@ public class PreferenceUtils {
             editor = getPreferences(context).edit();
         }
         return editor;
-    }
-
-    public static void setLastCourseUiUsedIsToc(@NonNull Context context, boolean isTocUi) {
-        getPreferenceEditor(context).putBoolean(LAST_COURSE_UI_USED_IS_TOC, isTocUi).apply();
-    }
-
-    public static boolean isTocUsedAsLastCourseUi(@NonNull Context context) {
-        return getPreferences(context).getBoolean(LAST_COURSE_UI_USED_IS_TOC, true);
     }
 
 }
