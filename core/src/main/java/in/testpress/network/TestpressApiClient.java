4e13dd4f649ab236548d1031eb1ac8c1e46e655c
Karthik <karthik@testpress.in>
2019-07-26 11:33:26 +0530

    Call logout API on logout

--- core/src/main/java/in/testpress/network/TestpressApiClient.java
+++ core/src/main/java/in/testpress/network/TestpressApiClient.java
@@ -48,7 +48,7 @@ public class TestpressApiClient {
     public static final String PROFILE_DETAILS_PATH= "api/v2.2/me/";
     public static final String ACCOUNT_ACTIVITY_PATH = "api/v2.3/me/login_activity/";
     public static final String LOGOUT_DEVICES = "api/v2.4/auth/logout_devices/";
-    public static final String LOGOUT_PATH = "api/v2.4/logout/";
+    public static final String LOGOUT_PATH = "api/v2.4/auth/logout/";
 
     /**
      * Query Params
@@ -208,6 +208,10 @@ public class TestpressApiClient {
 
     public RetrofitCall<ProfileDetails> getProfileDetails() {
         return getAuthenticationService().getProfileDetails();
+    }
+
+    public RetrofitCall<Void> logout() {
+        return getAuthenticationService().logout();
     }
 
     public RetrofitCall<FileDetails> upload(String filePath) {
