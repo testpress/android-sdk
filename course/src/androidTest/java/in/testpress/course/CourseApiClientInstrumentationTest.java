f8fbf7a9a8dc2e65c9084ffb8e0299b5d5bea42a
Karthik <karthik@testpress.in>
2019-10-17 14:25:10 +0530

    Fix test case

--- course/src/androidTest/java/in/testpress/course/CourseApiClientInstrumentationTest.java
+++ course/src/androidTest/java/in/testpress/course/CourseApiClientInstrumentationTest.java
@@ -1,7 +1,8 @@
 package in.testpress.course;
 
-import android.support.test.InstrumentationRegistry;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import androidx.test.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
