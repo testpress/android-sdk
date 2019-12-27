247b24cc9cf656c31012965b1c9bf832227ec584
Karthik <karthik@testpress.in>
2019-12-27 11:15:49 +0530

    Add test case

---
+++ core/src/test/java/in/testpress/network/TestBaseResourcePager.java
@@ -0,0 +1,88 @@
+package in.testpress.network;
+
+import androidx.test.core.app.ApplicationProvider;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import java.io.IOException;
+import java.util.List;
+
+import in.testpress.core.TestpressCallback;
+import in.testpress.core.TestpressException;
+import in.testpress.core.TestpressSdk;
+import in.testpress.core.TestpressSession;
+import in.testpress.models.AccountActivity;
+import in.testpress.models.InstituteSettings;
+import in.testpress.models.TestpressApiResponse;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+
+@RunWith(RobolectricTestRunner.class)
+public class TestBaseResourcePager {
+    private class SamplePager extends BaseResourcePager<AccountActivity> {
+
+        private TestpressApiClient apiClient;;
+
+        public SamplePager(TestpressApiClient apiClient) {
+            this.apiClient = apiClient;
+        }
+
+        @Override
+        protected Object getId(AccountActivity resource) {
+            return resource.getId();
+        }
+
+        @Override
+        public RetrofitCall<TestpressApiResponse<AccountActivity>> getItems(
+                int page, int size) {
+            return apiClient.getAccountActivity(queryParams);
+        }
+
+    }
+
+    private MockWebServer mockWebServer;
+    private static final String USER_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6NDYsInVzZXJfaWQiOjQ2LCJlbWFpbCI6IiIsImV4cCI6MTUxOTAzNjUzM30.FUuyJfYNSAw_VcypZsN8_ZHvZra6gHU3njcXmr-TGVU";
+
+
+    @Before
+    public void setUp() throws IOException {
+        mockWebServer = new MockWebServer();
+        InstituteSettings instituteSettings =
+                new InstituteSettings("http://localhost:9200");
+        TestpressSdk.setTestpressSession(ApplicationProvider.getApplicationContext(),
+                new TestpressSession(instituteSettings, USER_TOKEN));
+        mockWebServer.start(9200);
+    }
+
+    @Test
+    public void testRetrofitCall() throws Exception{
+        TestpressApiClient apiClient = new TestpressApiClient(ApplicationProvider.getApplicationContext(), TestpressSdk.getTestpressSession(ApplicationProvider.getApplicationContext()));
+        SamplePager pager = new SamplePager(apiClient);
+        MockResponse successResponse = new MockResponse().setResponseCode(200);
+        mockWebServer.enqueue(successResponse);
+        pager.enqueueNext(new TestpressCallback<List<AccountActivity>>() {
+            @Override
+            public void onSuccess(List<AccountActivity> result) {
+
+            }
+
+            @Override
+            public void onException(TestpressException exception) {
+
+            }
+        });
+        mockWebServer.takeRequest();
+
+        assert pager.retrofitCall != null;
+    }
+
+    @After
+    public void tearDown() throws IOException {
+        mockWebServer.shutdown();
+    }
+
+}
