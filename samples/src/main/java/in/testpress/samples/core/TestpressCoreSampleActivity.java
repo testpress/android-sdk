c2ed80e94764f05b83d1b48a78ffa670edb4ef70
Karthik <karthik@testpress.in>
2020-02-10 10:43:27 +0530

    Release sdk version 1.3.64

--- samples/src/main/java/in/testpress/samples/core/TestpressCoreSampleActivity.java
+++ samples/src/main/java/in/testpress/samples/core/TestpressCoreSampleActivity.java
@@ -119,7 +119,7 @@ public class TestpressCoreSampleActivity extends BaseToolBarActivity {
     }
 
     private void authenticate(String userId, String accessToken, TestpressSdk.Provider provider) {
-        InstituteSettings instituteSettings = new InstituteSettings("https://surgerysixer.testpress.in");
+        InstituteSettings instituteSettings = new InstituteSettings("https://4iiteens.testpress.in");
         TestpressSdk.initialize(this, instituteSettings, userId, accessToken, provider,
                 new TestpressCallback<TestpressSession>() {
                     @Override
