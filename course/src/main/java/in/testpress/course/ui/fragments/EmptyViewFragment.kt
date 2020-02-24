0fe8e42f7992228614f6fcbffc541bb2ce64ce6a
Karthik raja <41418580+Karthik-0@users.noreply.github.com>
2020-02-24 14:16:34 +0530

    Add fragment to display exception (#157)
    - Added fragment to display error/exception
    - This can be reused in any activity/fragment to handle the exception.
    - This will reduce redundant code.

---
+++ course/src/main/java/in/testpress/course/ui/fragments/EmptyViewFragment.kt
@@ -0,0 +1,104 @@
+package `in`.testpress.course.ui.fragments
+
+import `in`.testpress.core.TestpressException
+import `in`.testpress.course.R
+import android.content.Context
+import android.os.Bundle
+import android.support.annotation.VisibleForTesting
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+
+
+class EmptyViewFragment : Fragment() {
+    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+    lateinit var emptyTitleView: TextView
+    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+    lateinit var emptyDescView: TextView
+    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+    lateinit var emptyContainer: LinearLayout
+    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+    lateinit var retryButton: Button
+
+    private var emptyViewListener: EmptyViewListener? = null
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.testpress_empty_view, container, false);
+    }
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        initializeListeners()
+    }
+
+    private fun initializeListeners() {
+        emptyViewListener = if (parentFragment != null) {
+            parentFragment as? EmptyViewListener
+        } else {
+            context as? EmptyViewListener
+        }
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        emptyContainer = view.findViewById(R.id.empty_container)
+        emptyTitleView = view.findViewById(R.id.empty_title)
+        emptyDescView = view.findViewById(R.id.empty_description)
+        retryButton = view.findViewById(R.id.retry_button)
+    }
+
+    fun displayError(exception: TestpressException) {
+        when {
+            exception.isForbidden -> handleForbidden()
+            exception.isNetworkError -> handleNetworkError()
+            exception.isPageNotFound -> handleIsPageNotFound()
+            else -> handleUnknownError()
+        }
+    }
+
+    private fun handleForbidden() {
+        setEmptyText(R.string.permission_denied,
+                R.string.testpress_no_permission,
+                R.drawable.ic_error_outline_black_18dp)
+        retryButton.visibility = View.GONE
+    }
+
+    private fun handleNetworkError() {
+        setEmptyText(R.string.testpress_network_error,
+                R.string.testpress_no_internet_try_again,
+                R.drawable.ic_error_outline_black_18dp)
+        retryButton.setOnClickListener {
+            emptyContainer.visibility = View.GONE
+            emptyViewListener?.onRetryClick()
+        }
+    }
+
+    private fun handleIsPageNotFound() {
+        setEmptyText(R.string.testpress_content_not_available,
+                R.string.testpress_content_not_available_description,
+                R.drawable.ic_error_outline_black_18dp)
+    }
+
+    private fun handleUnknownError() {
+        setEmptyText(R.string.testpress_error_loading_contents,
+                R.string.testpress_some_thing_went_wrong_try_again,
+                R.drawable.ic_error_outline_black_18dp)
+    }
+
+    private fun setEmptyText(title: Int, description: Int, left: Int) {
+        emptyContainer.visibility = View.VISIBLE
+        emptyTitleView.setText(title)
+        emptyTitleView.setCompoundDrawablesWithIntrinsicBounds(left, 0, 0, 0)
+        emptyDescView.setText(description)
+        retryButton.visibility = View.VISIBLE
+    }
+}
+
+interface EmptyViewListener {
+    fun onRetryClick()
+}
---
+++ course/src/test/java/in/testpress/course/fragments/EmptyViewFragmentTest.kt
@@ -0,0 +1,98 @@
+package `in`.testpress.course.fragments
+
+import `in`.testpress.core.TestpressException
+import `in`.testpress.course.R
+import `in`.testpress.course.ui.fragments.EmptyViewFragment
+import `in`.testpress.models.TestpressApiResponse
+import `in`.testpress.models.greendao.Content
+import android.content.Context
+import android.view.View
+import androidx.test.core.app.ApplicationProvider
+import junit.framework.Assert
+import okhttp3.MediaType
+import okhttp3.Protocol
+import okhttp3.Request
+import okhttp3.ResponseBody
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.shadows.support.v4.SupportFragmentTestUtil
+import retrofit2.Response
+import java.io.IOException
+
+
+@RunWith(RobolectricTestRunner::class)
+class EmptyViewFragmentTest {
+    lateinit var fragment: EmptyViewFragment
+    private val context = ApplicationProvider.getApplicationContext<Context>()
+
+    @Before
+    fun setUp() {
+        fragment = EmptyViewFragment()
+        SupportFragmentTestUtil.startFragment(fragment)
+    }
+
+    private fun buildErrorResponse(errorCode: Int): Response<TestpressApiResponse<Content>> {
+        val responseBody = ResponseBody.create(MediaType.parse("application/json"), """{"detail":"Error"}""")
+        return Response.error<TestpressApiResponse<Content>>(responseBody, okhttp3.Response.Builder()
+                .code(errorCode)
+                .message("Error")
+                .protocol(Protocol.HTTP_1_1)
+                .request(Request.Builder().url("http://localhost/").build())
+                .build())
+    }
+
+    @Test
+    fun testNetworkError() {
+        val ioException = IOException()
+        val exception = TestpressException.networkError(ioException)
+        fragment.displayError(exception)
+
+        Assert.assertEquals(View.VISIBLE, fragment.emptyContainer.visibility)
+        Assert.assertEquals(context.getString(R.string.testpress_network_error), fragment.emptyTitleView.text)
+        Assert.assertEquals(context.getString(R.string.testpress_no_internet_try_again), fragment.emptyDescView.text)
+    }
+
+    @Test
+    fun testForbiddenError() {
+        val exception = TestpressException.httpError(buildErrorResponse(403))
+        fragment.displayError(exception)
+
+        Assert.assertEquals(View.VISIBLE, fragment.emptyContainer.visibility)
+        Assert.assertEquals(context.getString(R.string.permission_denied), fragment.emptyTitleView.text)
+        Assert.assertEquals(context.getString(R.string.testpress_no_permission), fragment.emptyDescView.text)
+    }
+
+    @Test
+    fun testIsPageNotFound() {
+        val exception = TestpressException.httpError(buildErrorResponse(404))
+        fragment.displayError(exception)
+
+        Assert.assertEquals(View.VISIBLE, fragment.emptyContainer.visibility)
+        Assert.assertEquals(context.getString(R.string.testpress_content_not_available), fragment.emptyTitleView.text)
+        Assert.assertEquals(context.getString(R.string.testpress_content_not_available_description), fragment.emptyDescView.text)
+    }
+
+    @Test
+    fun testOtherErrorCode() {
+        val exception = TestpressException.httpError(buildErrorResponse(405))
+        fragment.displayError(exception)
+
+        Assert.assertEquals(View.VISIBLE, fragment.emptyContainer.visibility)
+        Assert.assertEquals(context.getString(R.string.testpress_error_loading_contents), fragment.emptyTitleView.text)
+        Assert.assertEquals(context.getString(R.string.testpress_some_thing_went_wrong_try_again), fragment.emptyDescView.text)
+    }
+
+    @Test
+    fun testRetryButtonClick() {
+        val ioException = IOException()
+        val exception = TestpressException.networkError(ioException)
+        fragment.displayError(exception)
+        Assert.assertEquals(View.VISIBLE, fragment.emptyContainer.visibility)
+
+        fragment.retryButton.performClick()
+
+        Assert.assertEquals(View.GONE, fragment.emptyContainer.visibility)
+    }
+}
