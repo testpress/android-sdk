7d6ad067e9c7080535345ec8ecff591621576cf3
Karthik <karthik@testpress.in>
2020-03-03 18:49:33 +0530

    Add injector file

--- course/src/main/java/in/testpress/course/di/InjectorUtils.kt
+++ course/src/main/java/in/testpress/course/di/InjectorUtils.kt
@@ -9,8 +9,9 @@ import android.content.Context
 object InjectorUtils {
     fun getContentRepository(context: Context): ContentRepository {
         val contentDao = TestpressSDKDatabase.getContentDao(context)
+        val attachmentDao = TestpressSDKDatabase.getAttachmentDao(context)
         val roomContentDao = TestpressDatabase(context).contentDao()
-        return ContentRepository(roomContentDao, contentDao, getCourseNetwork(context))
+        return ContentRepository(roomContentDao, contentDao, attachmentDao, getCourseNetwork(context))
     }
 
     private fun getCourseNetwork(context: Context): CourseNetwork {
