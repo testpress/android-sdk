8a8b395bb9b6c3a7e1078574222920cd295d94fb
Karthik <karthik@testpress.in>
2020-03-03 17:34:41 +0530

    Add API to create content attempt

--- course/src/main/java/in/testpress/course/network/CourseService.kt
+++ course/src/main/java/in/testpress/course/network/CourseService.kt
@@ -1,10 +1,12 @@
 package `in`.testpress.course.network
 
 import `in`.testpress.core.TestpressSdk
+import `in`.testpress.course.api.TestpressCourseApiClient
 import `in`.testpress.network.RetrofitCall
 import `in`.testpress.network.TestpressApiClient
 import android.content.Context
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface CourseService {
@@ -12,6 +14,11 @@ interface CourseService {
     fun getNetworkContent(
         @Path(value = "content_url", encoded = true) contentUrl: String
     ): RetrofitCall<NetworkContent>
+
+    @POST(TestpressCourseApiClient.CONTENTS_PATH + "{content_id}" + TestpressCourseApiClient.ATTEMPTS_PATH)
+    fun createContentAttempt(
+        @Path(value = "content_id", encoded = true) contentId: Long
+    ): RetrofitCall<NetworkContentAttempt>
 }
 
 
@@ -22,5 +29,9 @@ class CourseNetwork(context: Context) : TestpressApiClient(context, TestpressSdk
 
     fun getNetworkContent(url: String): RetrofitCall<NetworkContent> {
         return getCourseService().getNetworkContent(url)
+    }
+
+    fun createContentAttempt(contentId: Long): RetrofitCall<NetworkContentAttempt> {
+        return getCourseService().createContentAttempt(contentId)
     }
 }
