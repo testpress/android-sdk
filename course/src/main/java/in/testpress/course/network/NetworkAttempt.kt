99c9a60f6c9cfaa8bd50d8099b0a25c74df08b9c
Karthik <karthik@testpress.in>
2020-03-05 16:11:04 +0530

    Add network models

---
+++ course/src/main/java/in/testpress/course/network/NetworkAttempt.kt
@@ -0,0 +1,53 @@
+package `in`.testpress.course.network
+
+import `in`.testpress.models.greendao.Attempt
+
+data class NetworkAttempt(
+    val id: Long,
+    val url: String? = null,
+    val date: String? = null,
+    val totalQuestions: Int? = null,
+    val score: String? = null,
+    val rank: String? = null,
+    val maxRank: String? = null,
+    val reviewUrl: String? = null,
+    val questionsUrl: String? = null,
+    val correctCount: Int? = null,
+    val incorrectCount: Int? = null,
+    val lastStartedTime: String? = null,
+    val remainingTime: String? = null,
+    val timeTaken: String? = null,
+    val state: String? = null,
+    val percentile: String? = null,
+    val speed: Int? = null,
+    val accuracy: Int? = null,
+    val percentage: String? = null
+)
+
+fun createNetworkAttempt(attempt: NetworkAttempt): Attempt {
+    return Attempt(
+        attempt.url,
+        attempt.id,
+        attempt.date,
+        attempt.totalQuestions,
+        attempt.score,
+        attempt.rank,
+        attempt.maxRank,
+        attempt.reviewUrl,
+        attempt.questionsUrl,
+        attempt.correctCount,
+        attempt.incorrectCount,
+        attempt.lastStartedTime,
+        attempt.remainingTime,
+        attempt.timeTaken,
+        attempt.state,
+        attempt.percentile,
+        attempt.speed,
+        attempt.accuracy,
+        attempt.percentage
+    )
+}
+
+fun NetworkAttempt.asGreenDaoModel(): Attempt {
+    return createNetworkAttempt(this)
+}
