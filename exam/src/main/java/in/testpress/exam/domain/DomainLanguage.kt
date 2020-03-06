a6d3e1eb8c8f9814331f6193ea8e43acd456a4aa
Karthik <karthik@testpress.in>
2020-03-06 10:18:29 +0530

    Add more models

--- exam/src/main/java/in/testpress/exam/domain/DomainLanguage.kt
+++ exam/src/main/java/in/testpress/exam/domain/DomainLanguage.kt
@@ -1,5 +1,6 @@
 package `in`.testpress.course.domain
 
+import `in`.testpress.exam.network.NetworkLanguage
 import `in`.testpress.models.greendao.Language
 
 data class DomainLanguage(
@@ -18,6 +19,15 @@ fun createDomainLanguage(language: Language): DomainLanguage {
     )
 }
 
+fun createDomainLanguage(language: NetworkLanguage): DomainLanguage {
+    return DomainLanguage(
+        id = language.id,
+        title = language.title,
+        code = language.code,
+        examId = language.examId
+    )
+}
+
 fun List<Language>.asDomainLanguages(): List<DomainLanguage> {
     return this.map {
         it.asDomainLanguage()
@@ -26,4 +36,29 @@ fun List<Language>.asDomainLanguages(): List<DomainLanguage> {
 
 fun Language.asDomainLanguage(): DomainLanguage {
     return createDomainLanguage(this)
+}
+
+fun NetworkLanguage.asDomainLanguage(): DomainLanguage {
+    return createDomainLanguage(this)
+}
+
+fun List<NetworkLanguage>.toDomainLanguages(): List<DomainLanguage> {
+    return this.map {
+        createDomainLanguage(it)
+    }
+}
+
+fun createGreenDaoModel(language: DomainLanguage): Language {
+    return Language(
+        language.id,
+        language.code,
+        language.title,
+        language.examId
+    )
+}
+
+fun List<DomainLanguage>.toGreenDaoModels(): List<Language> {
+    return this.map {
+        createGreenDaoModel(it)
+    }
 }
