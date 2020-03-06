318bba7b23ec3b4ff77099ee01866208d8aacd40
Karthik <karthik@testpress.in>
2020-03-06 11:28:17 +0530

    Add API to get content attempts

--- course/src/main/java/in/testpress/course/network/CourseService.kt
+++ course/src/main/java/in/testpress/course/network/CourseService.kt
@@ -2,12 +2,14 @@ package `in`.testpress.course.network
 
 import `in`.testpress.core.TestpressSdk
 import `in`.testpress.course.api.TestpressCourseApiClient
+import `in`.testpress.models.TestpressApiResponse
 import `in`.testpress.network.RetrofitCall
 import `in`.testpress.network.TestpressApiClient
 import android.content.Context
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Path
+import retrofit2.http.Url
 
 interface CourseService {
     @GET("{content_url}")
@@ -19,13 +21,14 @@ interface CourseService {
     fun createContentAttempt(
         @Path(value = "content_id", encoded = true) contentId: Long
     ): RetrofitCall<NetworkContentAttempt>
+
+    @GET
+    fun getContentAttempts(@Url attemptsUrl: String): RetrofitCall<TestpressApiResponse<NetworkContentAttempt>>
 }
 
 
 class CourseNetwork(context: Context) : TestpressApiClient(context, TestpressSdk.getTestpressSession(context)) {
-    private fun getCourseService(): CourseService {
-        return retrofit.create(CourseService::class.java)
-    }
+    private fun getCourseService() = retrofit.create(CourseService::class.java)
 
     fun getNetworkContent(url: String): RetrofitCall<NetworkContent> {
         return getCourseService().getNetworkContent(url)
@@ -33,5 +36,9 @@ class CourseNetwork(context: Context) : TestpressApiClient(context, TestpressSdk
 
     fun createContentAttempt(contentId: Long): RetrofitCall<NetworkContentAttempt> {
         return getCourseService().createContentAttempt(contentId)
+    }
+
+    fun getContentAttempts(url: String): RetrofitCall<TestpressApiResponse<NetworkContentAttempt>> {
+        return getCourseService().getContentAttempts(url)
     }
 }
