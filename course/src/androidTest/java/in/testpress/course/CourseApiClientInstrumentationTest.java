7490172f4f229d171fd77f4a2fab57e0eb5c9714
Karthik <karthik@testpress.in>
2020-03-06 11:54:47 +0530

    Use androidx compliant

--- course/src/androidTest/java/in/testpress/course/CourseApiClientInstrumentationTest.java
+++ course/src/androidTest/java/in/testpress/course/CourseApiClientInstrumentationTest.java
@@ -1,6 +1,6 @@
 package in.testpress.course;
 
-import android.support.test.InstrumentationRegistry;
+import androidx.test.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Test;
