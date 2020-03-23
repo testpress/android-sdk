6a3c6da32ee1a00c048cb031bfffc218c40f6340
Karthik <karthik@testpress.in>
2020-03-23 12:02:41 +0530

    Refactor code

--- core/src/main/java/in/testpress/ui/BaseToolBarActivity.java
+++ core/src/main/java/in/testpress/ui/BaseToolBarActivity.java
@@ -8,11 +8,15 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.MenuItem;
 import android.widget.ImageView;
 
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+
 import in.testpress.R;
 import in.testpress.core.TestpressSdk;
 import in.testpress.core.TestpressSession;
 import in.testpress.network.RetrofitCall;
 import in.testpress.util.CommonUtils;
+import in.testpress.util.ImageUtils;
 
 import static android.view.WindowManager.LayoutParams.FLAG_SECURE;
 import static in.testpress.core.TestpressSdk.ACTION_PRESSED_HOME;
@@ -26,10 +30,11 @@ public abstract class BaseToolBarActivity extends AppCompatActivity {
 
     public static final String ACTIONBAR_TITLE = "title";
     protected ImageView logo;
+    private TestpressSession session;
 
     @Override
     public void setContentView(final int layoutResId) {
-        TestpressSession session = TestpressSdk.getTestpressSession(this);
+        session = TestpressSdk.getTestpressSession(this);
         if (session != null && session.getInstituteSettings().isScreenshotDisabled()) {
             getWindow().setFlags(FLAG_SECURE, FLAG_SECURE);
         }
@@ -50,6 +55,20 @@ public abstract class BaseToolBarActivity extends AppCompatActivity {
     public void setActionBarTitle(String title) {
         //noinspection ConstantConditions
         getSupportActionBar().setTitle(title);
+    }
+
+    public void setLogo() {
+        getSupportActionBar().setTitle("");
+        if (session == null || session.getInstituteSettings() == null) {
+            return;
+        }
+        String url = session.getInstituteSettings().getAppToolbarLogo();
+        ImageLoader imageLoader = ImageUtils.initImageLoader(this);
+        DisplayImageOptions options = new DisplayImageOptions.Builder()
+                .cacheInMemory(true)
+                .cacheOnDisk(true)
+                .build();
+        imageLoader.displayImage(url, logo, options);
     }
 
     @SuppressWarnings("DanglingJavadoc")
