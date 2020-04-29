050d5020552dce98b61f6425cd4524c1cac4bc80
Karthik <karthik@testpress.in>
2020-04-29 17:07:28 +0530

    Fix image click not working in review questions

--- core/src/main/assets/TestpressImageTagHandler.js
+++ core/src/main/assets/TestpressImageTagHandler.js
@@ -1,12 +1,6 @@
 // Set click listeners to the images in the html content to display the images as zoomable
 var images = document.getElementsByTagName("img");
 for (i = 0; i < images.length; i++) {
-   images[i].onclick = (
-       function() {
-           var src = images[i].src;
-           return function() {
-               ImageHandler.onClickImage(src);
-           }
-       }
-   )();
+    var src = images[i].src;
+    images[i].setAttribute('onclick','ImageHandler.onClickImage(src)');
 }
