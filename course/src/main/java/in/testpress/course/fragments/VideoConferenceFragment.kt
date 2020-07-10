3901148b7c6d5c378bf22bb2014a3f93636aca05
Karthik raja <41418580+Karthik-0@users.noreply.github.com>
2020-07-10 11:14:36 +0530

    Integrate zoom SDK for video conference (#232)
    - Integrated Zoom android SDK for video conference

--- course/src/main/java/in/testpress/course/fragments/VideoConferenceFragment.kt
+++ course/src/main/java/in/testpress/course/fragments/VideoConferenceFragment.kt
@@ -1,8 +1,12 @@
 package `in`.testpress.course.fragments
 
 import `in`.testpress.core.TestpressSdk
+import `in`.testpress.core.TestpressUserDetails
 import `in`.testpress.course.R
+import `in`.testpress.course.domain.DomainVideoConferenceContent
 import `in`.testpress.course.ui.WebViewActivity
+import `in`.testpress.course.util.VideoConferenceHandler
+import `in`.testpress.models.ProfileDetails
 import `in`.testpress.util.ViewUtils
 import android.content.Intent
 import android.os.Bundle
@@ -20,6 +24,16 @@ class VideoConferenceFragment : BaseContentDetailFragment() {
     private lateinit var duration: TextView
     private lateinit var startTime: TextView
     private lateinit var provider: TextView
+    private var videoConferenceHandler: VideoConferenceHandler? = null
+    private var profileDetails: ProfileDetails? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        profileDetails = TestpressUserDetails.getInstance().profileDetails
+        if (profileDetails == null) {
+            TestpressUserDetails.getInstance().load(requireContext())
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -49,6 +63,7 @@ class VideoConferenceFragment : BaseContentDetailFragment() {
 
     override fun display() {
         val videoConference = content.videoConference
+        initializeVideoConferenceHandler(videoConference)
 
         titleView.text = content.title
         titleLayout.visibility = View.VISIBLE
@@ -58,8 +73,22 @@ class VideoConferenceFragment : BaseContentDetailFragment() {
             startTime.text = it.formattedStartDate()
         }
         startButton.setOnClickListener {
-            openVideoConferenceInWebView()
+            joinMeeting()
         }
+    }
+
+    private fun initializeVideoConferenceHandler(videoConference: DomainVideoConferenceContent?) {
+        videoConferenceHandler =  VideoConferenceHandler(requireContext(), videoConference!!, profileDetails)
+        videoConferenceHandler?.init()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        videoConferenceHandler?.destroy()
+    }
+
+    private fun joinMeeting() {
+        videoConferenceHandler?.joinMeet()
     }
 
     private fun openVideoConferenceInWebView() {
