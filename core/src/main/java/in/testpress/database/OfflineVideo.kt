334aa236d240938e54db0d2bf7928cec436303f2
Karthik raja <41418580+Karthik-0@users.noreply.github.com>
2020-07-25 10:40:22 +0530

    Add offline video model to store downloaded video details (#242)
    -  Added room model entity to store data related to video and its download progress, state

---
+++ core/src/main/java/in/testpress/database/OfflineVideo.kt
@@ -0,0 +1,20 @@
+package `in`.testpress.database
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class OfflineVideo(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long? = null,
+    val title: String? = null,
+    val description: String? = null,
+    val remoteThumbnail: String? = null,
+    val localThumbnail: String? = null,
+    val duration: String = "00:00:00",
+    val url: String? = null,
+    val contentId: Long? = null,
+    var percentageDownloaded: Int = 0,
+    var bytesDownloaded: Long = 0,
+    var totalSize: Long = 0
+)
