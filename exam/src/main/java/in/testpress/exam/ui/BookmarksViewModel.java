1bed361bd641bfd5671554d1fa218819828a25e6
sangeetha <sangeetha@testpress.in>
2020-07-31 09:17:57 +0530

    Code Cleanup

--- exam/src/main/java/in/testpress/exam/ui/BookmarksViewModel.java
+++
@@ -1,9 +0,0 @@
-package in.testpress.exam.ui;
-
-import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
-
-public class BookmarksViewModel extends ViewModel {
-
-    public MutableLiveData<Boolean> isHidden = new MutableLiveData<>();
-}
