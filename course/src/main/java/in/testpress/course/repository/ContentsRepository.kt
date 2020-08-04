a4464490c7a6aa1fca5a1d9dac194d50cd5d7982
Karthik <karthik@testpress.in>
2020-08-04 20:34:32 +0530

    Fix contents list page is not showing from db on network error
    - Displayed contents list from db in case of network error

--- course/src/main/java/in/testpress/course/repository/ContentsRepository.kt
+++ course/src/main/java/in/testpress/course/repository/ContentsRepository.kt
@@ -50,7 +50,12 @@ class ContentsRepository(val context: Context, val chapterId: Long = -1) {
                 }
 
                 override fun onException(exception: TestpressException?) {
-                    _resourceContents.postValue(Resource.error(exception!!, null))
+                    val contents = getAll()
+                    if (contents?.isNotEmpty() == true) {
+                        _resourceContents.postValue(Resource.error(exception!!, contents.asDomainContents()))
+                    } else {
+                        _resourceContents.postValue(Resource.error(exception!!, null))
+                    }
                 }
             })
     }
@@ -80,6 +85,12 @@ class ContentsRepository(val context: Context, val chapterId: Long = -1) {
             .where(ContentDao.Properties.ChapterId.eq(chapterId))
             .buildDelete()
             .executeDeleteWithoutDetachingEntities()
+    }
+
+    private fun getAll(): MutableList<Content>? {
+        return contentDao.queryBuilder()
+            .where(ContentDao.Properties.ChapterId.eq(chapterId))
+            .list()
     }
 
     private fun storeContent(response: ContentsListResponse): List<DomainContent> {
