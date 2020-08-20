2632b8859a4d09fe8acc51a26a0fd1bbfb33e385
Sangeetha <sakthivelsangeetha99@gmail.com>
2020-08-20 12:28:24 +0530

    Add Click Listeners

---
+++ exam/src/main/java/in/testpress/exam/ui/CommentsFragment.kt
@@ -0,0 +1,33 @@
+package `in`.testpress.exam.ui
+
+import `in`.testpress.exam.R
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import kotlinx.android.synthetic.main.fragment_comments.*
+
+class CommentsFragment : DialogFragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NORMAL, R.style.FullScreenDialogTheme)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_comments, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setOnClickListeners()
+    }
+
+    private fun setOnClickListeners() {
+        closeButton.setOnClickListener {
+            dismiss()
+        }
+    }
+}
