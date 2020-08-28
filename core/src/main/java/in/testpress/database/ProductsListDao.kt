173755230954e9c20e729b74b8684dad5ed52691
Sangeetha <sakthivelsangeetha99@gmail.com>
2020-08-28 12:20:52 +0530

    Add Product List Room

---
+++ core/src/main/java/in/testpress/database/ProductsListDao.kt
@@ -0,0 +1,15 @@
+package `in`.testpress.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+
+@Dao
+interface ProductsListDao: BaseDao<ProductsListEntity> {
+    @Transaction
+    @Query("SELECT * FROM productslistentity")
+    fun getAll(): LiveData<ProductsListEntity>
+
+    @Query("SELECT * from productslistentity where id = :id LIMIT 1")
+    fun findById(id: Long): LiveData<ProductsListEntity>
+}
