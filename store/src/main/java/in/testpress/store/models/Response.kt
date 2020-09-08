e88f812a0ece74957af81ee789393d1bc3a4b7b0
Sangeetha <sangeetha@testpress.in>
2020-09-08 21:13:15 +0530

    Rebase

--- store/src/main/java/in/testpress/store/models/Response.kt
+++ store/src/main/java/in/testpress/store/models/Response.kt
@@ -1,29 +1,46 @@
 package `in`.testpress.store.models
 
-data class ProductsList(
-	val courses: List<CoursesItem?>? = null,
-	val prices: List<PricesItem?>? = null,
-	val products: List<ProductsItem?>? = null
+data class Response(
+	val next: String? = null,
+	val perPage: Int? = null,
+	val previous: String? = null,
+	val count: Int? = null,
+	val results: Result? = null
 )
 
-data class ProductsItem(
+data class Result(
+	val courses: List<Courses?>? = null,
+	val prices: List<Prices?>? = null,
+	val products: List<Products?>? = null
+)
+
+data class Prices(
 	val endDate: String? = null,
+	val price: String? = null,
+	val name: String? = null,
+	val id: Int? = null,
+	val validity: Any? = null,
+	val startDate: String? = null
+)
+
+data class Products(
+	val endDate: Any? = null,
 	val image: String? = null,
-	val courses: List<Int?>? = null,
-	val surl: String? = null,
+	val courses: List<Any?>? = null,
+	val surl: Any? = null,
 	val title: String? = null,
 	val paymentLink: String? = null,
 	val buyNowText: String? = null,
-	val furl: String? = null,
+	val furl: Any? = null,
 	val id: Int? = null,
 	val descriptionHtml: String? = null,
 	val currentPrice: String? = null,
 	val prices: List<Int?>? = null,
 	val slug: String? = null,
-	val startDate: String? = null
+	val startDate: Any? = null
 )
 
-data class CoursesItem(
+data class Courses(
 	val image: String? = null,
 	val examsCount: Int? = null,
 	val created: String? = null,
@@ -47,3 +64,4 @@ data class CoursesItem(
 	val order: Int? = null,
 	val externalLinkLabel: String? = null
 )
+
