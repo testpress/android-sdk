744a42efdcc2b35af8983101d1f55ab44b6855e3
Sangeetha <sangeetha@testpress.in>
2020-10-08 13:29:46 +0530

    Refactor Room Migration 4 to 5

--- core/src/androidTest/java/in/testpress/core/database/RoomMigration4To5Test.kt
+++ core/src/androidTest/java/in/testpress/core/database/RoomMigration4To5Test.kt
@@ -4,7 +4,6 @@ import `in`.testpress.database.TestpressDatabase
 import `in`.testpress.database.roommigration.RoomMigration4To5.MIGRATION_4_5
 import androidx.room.Room
 import androidx.room.testing.MigrationTestHelper
-import androidx.sqlite.db.SupportSQLiteDatabase
 import androidx.sqlite.db.framework.FrameworkSQLiteOpenHelperFactory
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
@@ -12,37 +11,44 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.jupiter.api.Assertions.assertDoesNotThrow
 import org.junit.runner.RunWith
-import java.io.IOException
-
 
 @RunWith(AndroidJUnit4::class)
 class RoomMigration4To5Test {
-
-    private val testPressDatabase = "testpress-database"
 
     @Rule @JvmField
     val helper: MigrationTestHelper = MigrationTestHelper(InstrumentationRegistry.getInstrumentation(),
             TestpressDatabase::class.java.canonicalName,
             FrameworkSQLiteOpenHelperFactory())
 
+    private val MIGRATIONS = arrayOf(MIGRATION_4_5)
+
+    private val testPressDatabase = "testpress-database"
+
+    private val appDb: TestpressDatabase = Room.databaseBuilder(
+            InstrumentationRegistry.getInstrumentation().targetContext,
+            TestpressDatabase::class.java,
+            testPressDatabase)
+            .addMigrations(*MIGRATIONS).build()
+
+    private val databaseCurrentVersion = appDb.openHelper.readableDatabase.version
 
     @Test
-    @Throws(IOException::class)
     fun migrationShouldRunSuccessfully() {
-        createAndCloseDatabaseWithVersion4()
         assertDoesNotThrow {
-            val appDb: TestpressDatabase = Room.databaseBuilder(
-                    InstrumentationRegistry.getInstrumentation().targetContext,
-                    TestpressDatabase::class.java,
-                    testPressDatabase)
-                    .addMigrations(MIGRATION_4_5).build()
-            appDb.openHelper.writableDatabase
-            appDb.close()
+            createDatabaseWithPreviousVersion()
+            createDatabaseWithLatestVersion()
         }
     }
 
-    private fun createAndCloseDatabaseWithVersion4() {
-        val db: SupportSQLiteDatabase = helper.createDatabase(testPressDatabase, 4)
-        db.close()
+    private fun createDatabaseWithPreviousVersion() {
+        helper.createDatabase(testPressDatabase, databaseCurrentVersion - 1).apply {
+            close()
+        }
+    }
+
+    private fun createDatabaseWithLatestVersion() {
+        helper.createDatabase(testPressDatabase, databaseCurrentVersion).apply {
+            close()
+        }
     }
 }
