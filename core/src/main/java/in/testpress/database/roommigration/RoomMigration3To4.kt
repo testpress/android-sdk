d77ef8f7afdc42fad8cf707228816b2098398024
Sangeetha <sangeetha@testpress.in>
2020-10-30 12:12:44 +0530

    Add ContentEntity in Room Migration 3 to 4
    Since the content entity schema is changed in the room version 4, contentEntity is added in room migration 3 to 4

--- core/src/main/java/in/testpress/database/roommigration/RoomMigration3To4.kt
+++ core/src/main/java/in/testpress/database/roommigration/RoomMigration3To4.kt
@@ -7,7 +7,16 @@ object RoomMigration3To4 {
 
     val MIGRATION_3_4: Migration = object : Migration(3, 4) {
         override fun migrate(database: SupportSQLiteDatabase) {
-            //Empty Migration
+            database.execSQL("DROP TABLE IF EXISTS ContentEntity")
+            database.execSQL(getContentEntityTableCreationSQL())
         }
+    }
+
+    fun getContentEntityTableCreationSQL(): String {
+        return "CREATE TABLE IF NOT EXISTS ContentEntity (id INTEGER NOT NULL, title TEXT, description TEXT, image TEXT, order INTEGER," +
+                " url TEXT NOT NULL, chapterId INTEGER, chapterSlug TEXT NOT NULL, chapterUrl TEXT, courseId INTEGER, freePreview INTEGER, " +
+                "modified TEXT, contentType TEXT NOT NULL, examUrl TEXT, videoUrl TEXT, attachmentUrl TEXT, htmlUrl TEXT, isLocked INTEGER NOT NULL," +
+                "isScheduled INTEGER NOT NULL, attemptsCount INTEGER NOT NULL, bookmarkId INTEGER, videoWatchedPercentage INTEGER, active INTEGER NOT NULL," +
+                "examId INTEGER, attachmentId INTEGER, videoId INTEGER, htmlId INTEGER, start TEXT, hasStarted INTEGER NOT NULL, isCourseAvailable INTEGER, PRIMARY KEY(id))"
     }
 }
