69c02dd062e873c7f7e3cb76b369fa754b58c8cc
Sangeetha <sangeetha@testpress.in>
2020-11-09 15:52:57 +0530

    Add Comment Network

---
+++ exam/src/main/java/in/testpress/exam/network/service/CommentService.kt
@@ -0,0 +1,68 @@
+package `in`.testpress.exam.network.service
+
+import `in`.testpress.core.TestpressSdk
+import `in`.testpress.exam.api.TestpressExamApiClient.*
+import `in`.testpress.exam.models.Vote
+import `in`.testpress.exam.network.NetworkComment
+import `in`.testpress.exam.network.NetworkCommentResponse
+import `in`.testpress.network.RetrofitCall
+import `in`.testpress.network.TestpressApiClient
+import android.content.Context
+import retrofit2.http.*
+import java.util.*
+
+@JvmSuppressWildcards
+interface CommentService {
+
+    @GET
+    fun getComments(@Url commentsUrl: String?,
+                    @QueryMap options: Map<String?, Any?>?): RetrofitCall<NetworkCommentResponse>
+
+    @POST
+    fun postComment(@Url commentsUrl: String?,
+                    @Body arguments: HashMap<String, String>): RetrofitCall<NetworkComment>
+
+    @POST(VOTES_PATH)
+    fun voteComment(@Body params: HashMap<String, Any>): RetrofitCall<Vote<NetworkComment>>
+
+    @DELETE("$VOTES_PATH{vote_id}/")
+    fun deleteCommentVote(@Path(value = "vote_id") id: Long?): RetrofitCall<String>
+
+    @PUT("$VOTES_PATH{vote_id}/")
+    fun updateCommentVote(
+            @Path(value = "vote_id") id: Long?,
+            @Body params: HashMap<String, Any>): RetrofitCall<Vote<NetworkComment>>
+}
+
+open class CommentApiClient(context: Context): TestpressApiClient(context, TestpressSdk.getTestpressSession(context)) {
+    private fun getService() = retrofit.create(CommentService::class.java)
+
+    fun getComments(urlFrag: String?,
+                    queryParams: Map<String?, Any?>?): RetrofitCall<NetworkCommentResponse> {
+        return getService().getComments(urlFrag, queryParams)
+    }
+
+    fun postComment(urlFrag: String?, comment: String): RetrofitCall<NetworkComment> {
+        val params = HashMap<String, String>()
+        params["comment"] = comment
+        return getService().postComment(urlFrag, params)
+    }
+
+    fun voteComment(comment: NetworkComment, typeOfVote: Int): RetrofitCall<Vote<NetworkComment>> {
+        val params = HashMap<String, Any>()
+        params["content_object"] = comment
+        params["type_of_vote"] = typeOfVote
+        return getService().voteComment(params)
+    }
+
+    fun deleteCommentVote(comment: NetworkComment): RetrofitCall<String> {
+        return getService().deleteCommentVote(comment.voteId)
+    }
+
+    fun updateCommentVote(comment: NetworkComment, typeOfVote: Int): RetrofitCall<Vote<NetworkComment>> {
+        val params = HashMap<String, Any>()
+        params["content_object"] = comment
+        params["type_of_vote"] = typeOfVote
+        return getService().updateCommentVote(comment.voteId, params)
+    }
+}
---
+++ exam/src/test/java/in/testpress/exam/network/CommentApiClientTest.kt
@@ -0,0 +1,52 @@
+package `in`.testpress.exam.network
+
+import kotlinx.coroutines.runBlocking
+import okhttp3.mockwebserver.MockResponse
+import org.junit.Assert
+import org.junit.Test
+
+class CommentApiClientTest: NetworkTestMixin() {
+
+    private val commentsJson = getResponseFromFile("comments.json")
+
+    @Test
+    fun successNetworkCallShouldReturnSuccessResponse() {
+        val successResponse = MockResponse().setResponseCode(200).setBody(commentsJson)
+        mockWebServer.enqueue(successResponse)
+
+        runBlocking {
+            val response = service.getComments("url", mapOf()).execute()
+            mockWebServer.takeRequest()
+
+            Assert.assertTrue(response.isSuccessful)
+        }
+    }
+
+    @Test
+    fun networkFailureShouldReturnException() {
+        val failureResponse = MockResponse().setStatus("Exception").setResponseCode(404).setBody(commentsJson)
+        mockWebServer.enqueue(failureResponse)
+
+        runBlocking {
+            val response = service.getComments("url", mapOf()).execute()
+            mockWebServer.takeRequest()
+
+            Assert.assertEquals(404, response.code())
+            Assert.assertEquals("HTTP/1.1 404 Client Error", failureResponse.status)
+        }
+    }
+
+    @Test
+    fun getCommentsShouldFetchComments() {
+        val successResponse = MockResponse().setResponseCode(200).setBody(commentsJson)
+        mockWebServer.enqueue(successResponse)
+
+        runBlocking {
+            val response = service.getComments("url", mapOf()).execute()
+            mockWebServer.takeRequest()
+
+            Assert.assertEquals("Hey", response.body().results?.get(0)?.comment)
+            Assert.assertEquals("may 05 2020", response.body().results?.get(0)?.submitDate)
+        }
+    }
+}
