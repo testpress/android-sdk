2297753a88fb0abbc213f81c483b5ad23d13cad0
Sangeetha <sangeetha@testpress.in>
2020-11-26 21:45:29 +0530

    Add Cover image view

--- course/src/main/java/in/testpress/course/ui/viewholders/VideoContentListItemViewHolder.kt
+++ course/src/main/java/in/testpress/course/ui/viewholders/VideoContentListItemViewHolder.kt
@@ -7,20 +7,16 @@ import `in`.testpress.course.domain.DomainVideoContent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import android.widget.LinearLayout
+import android.widget.ProgressBar
 import android.widget.TextView
-import androidx.core.content.ContextCompat
-import com.github.testpress.mikephil.charting.charts.PieChart
-import com.github.testpress.mikephil.charting.data.PieData
-import com.github.testpress.mikephil.charting.data.PieDataSet
-import com.github.testpress.mikephil.charting.data.PieEntry
-import java.util.*
 
 class VideoContentListItemViewHolder(view: View) : BaseContentListItemViewHolder(view) {
     private val duration: TextView = view.findViewById(R.id.duration)
     private val durationContainer: LinearLayout = view.findViewById(R.id.duration_container)
-    private val videoCompletionProgressContainer: LinearLayout = view.findViewById(R.id.video_completion_progress_container)
-    private var progressChart: PieChart = view.findViewById(R.id.video_completion_progress_chart)
+    private val videoCompletionProgressContainer: FrameLayout = view.findViewById(R.id.video_completion_progress_container)
+    private var progressbar: ProgressBar = view.findViewById(R.id.video_completion_progress)
 
     init {
         duration.typeface = TestpressSdk.getRubikMediumFont(view.context)
@@ -36,7 +32,6 @@ class VideoContentListItemViewHolder(view: View) : BaseContentListItemViewHolder
     private fun bindVideoProgress(content: DomainContent) {
         attemptedTickContainer.visibility = View.GONE
         videoCompletionProgressContainer.visibility = View.GONE
-
         when (content.videoWatchedPercentage) {
             0, null -> {}
             100 ->  attemptedTickContainer.visibility = View.VISIBLE
@@ -46,19 +41,7 @@ class VideoContentListItemViewHolder(view: View) : BaseContentListItemViewHolder
 
     private fun showVideoProgressPercentage(videoWatchedPercentage: Int) {
         videoCompletionProgressContainer.visibility = View.VISIBLE
-        progressChart.setDescription("")
-        progressChart.isClickable = false
-        progressChart.data = getVideoProgressPieChartData(videoWatchedPercentage)
-        progressChart.setTouchEnabled(false)
-        progressChart.setUsePercentValues(true)
-        progressChart.centerText = "$videoWatchedPercentage%"
-        progressChart.setCenterTextSize(7f)
-        progressChart.setCenterTextTypeface(TestpressSdk.getRubikMediumFont(itemView.context))
-        progressChart.setCenterTextColor(ContextCompat.getColor(itemView.context, R.color.testpress_text_gray))
-        progressChart.holeRadius = 85f
-        progressChart.transparentCircleRadius = 0f
-        progressChart.setExtraOffsets(0f, 0f, 0f, 0f)
-        progressChart.legend.isEnabled = false
+        progressbar.progress = videoWatchedPercentage
     }
 
     private fun bindDuration(it: DomainVideoContent) {
@@ -68,20 +51,6 @@ class VideoContentListItemViewHolder(view: View) : BaseContentListItemViewHolder
             duration.text = it.duration
             durationContainer.visibility = View.VISIBLE
         }
-    }
-
-    private fun getVideoProgressPieChartData(watchedDuration: Int): PieData? {
-        val entries = ArrayList<PieEntry>()
-        entries.add(PieEntry(watchedDuration.toFloat(), 0))
-        entries.add(PieEntry((100 - watchedDuration).toFloat(), 1))
-        val dataSet = PieDataSet(entries, "")
-        val colors = ArrayList<Int>()
-        colors.add(ContextCompat.getColor(itemView.context, R.color.testpress_green))
-        colors.add(ContextCompat.getColor(itemView.context, R.color.testpress_gray_light))
-        dataSet.colors = colors
-        val data = PieData(dataSet)
-        data.setDrawValues(false)
-        return data
     }
 
     companion object {
