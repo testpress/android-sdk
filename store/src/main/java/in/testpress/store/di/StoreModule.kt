04c7c3b3118cecb6061021b96b6e893739f293a3
Sangeetha <sangeetha@testpress.in>
2020-12-07 11:49:28 +0530

    Add Dagger hilt for store

---
+++ store/src/main/java/in/testpress/store/di/StoreModule.kt
@@ -0,0 +1,10 @@
+package `in`.testpress.store.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object StoreModule {
+}
