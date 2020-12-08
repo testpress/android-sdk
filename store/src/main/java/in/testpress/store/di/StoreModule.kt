5c4e84495f6e54ad4393e7f633c61d15dab3cde9
Sangeetha <sangeetha@testpress.in>
2020-12-08 18:49:55 +0530

    Add Product list

--- store/src/main/java/in/testpress/store/di/StoreModule.kt
+++ store/src/main/java/in/testpress/store/di/StoreModule.kt
@@ -3,7 +3,11 @@ package `in`.testpress.store.di
 import `in`.testpress.database.ProductDao
 import `in`.testpress.database.TestpressDatabase
 import `in`.testpress.store.network.StoreApiClient
+import `in`.testpress.store.repository.ProductsRepository
+import `in`.testpress.store.ui.ProductsListActivity
+import `in`.testpress.store.viewmodel.ProductListViewModel
 import android.content.Context
+import androidx.lifecycle.SavedStateHandle
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -25,5 +29,15 @@ object StoreModule {
     @Provides
     fun provideStoreApiClient(@ApplicationContext context: Context): StoreApiClient {
         return StoreApiClient(context)
+    }
+
+    @Provides
+    fun provideProductListRepository(productDao: ProductDao, storeApiClient: StoreApiClient): ProductsRepository {
+        return ProductsRepository(productDao, storeApiClient)
+    }
+
+    @Provides
+    fun provideViewModel(repository: ProductsRepository): ProductListViewModel {
+        return ProductListViewModel(repository, SavedStateHandle())
     }
 }
---
+++ store/src/main/java/in/testpress/store/view/activity/ProductListActivity.kt
@@ -0,0 +1,9 @@
+package `in`.testpress.store.view.activity
+
+import `in`.testpress.ui.BaseToolBarActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ProductListActivity: BaseToolBarActivity() {
+
+}
