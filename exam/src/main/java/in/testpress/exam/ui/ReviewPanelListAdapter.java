2ab4d539caea2f904e3e3a70df7a9e7a7ba50f94
Sangeetha <sangeetha@testpress.in>
2020-12-18 19:40:44 +0530

    Add webview to display question list

--- exam/src/main/java/in/testpress/exam/ui/ReviewPanelListAdapter.java
+++ exam/src/main/java/in/testpress/exam/ui/ReviewPanelListAdapter.java
@@ -1,20 +1,23 @@
 package in.testpress.exam.ui;
 
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.graphics.Color;
-import androidx.core.content.ContextCompat;
-import android.text.Html;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
-
+import androidx.core.content.ContextCompat;
 import java.util.List;
-
 import in.testpress.exam.R;
+import in.testpress.exam.ui.view.WebView;
 import in.testpress.models.greendao.Language;
 import in.testpress.models.greendao.ReviewItem;
 import in.testpress.models.greendao.ReviewQuestion;
 import in.testpress.models.greendao.ReviewQuestionTranslation;
 import in.testpress.util.SingleTypeAdapter;
-
+import in.testpress.util.WebViewUtils;
 import static in.testpress.models.greendao.ReviewItem.ANSWERED_INCORRECT;
 import static in.testpress.models.greendao.ReviewItem.UNANSWERED;
 
@@ -25,9 +28,14 @@ class ReviewPanelListAdapter extends SingleTypeAdapter<ReviewItem> {
 
     private int currentItemPosition = 1;
     private Language selectedLanguage;
+    private final Activity activity;
+    private final ListItemClickListener listItemClickListener;
 
-    ReviewPanelListAdapter(final LayoutInflater inflater, final List<ReviewItem> items, int layout) {
+    ReviewPanelListAdapter(final LayoutInflater inflater, final List<ReviewItem> items, int layout,
+                           Activity activity,  ListItemClickListener listItemClickListener) {
         super(inflater, layout);
+        this.activity = activity;
+        this.listItemClickListener = listItemClickListener;
         setItems(items);
     }
 
@@ -40,6 +48,7 @@ class ReviewPanelListAdapter extends SingleTypeAdapter<ReviewItem> {
     }
 
     @Override
+    @SuppressLint("ClickableViewAccessibility")
     protected void update(final int position, final ReviewItem item) {
         String question = null;
         ReviewQuestion reviewQuestion = item.getQuestion();
@@ -49,20 +58,18 @@ class ReviewPanelListAdapter extends SingleTypeAdapter<ReviewItem> {
 
             for (ReviewQuestionTranslation translation : translations) {
                 if (translation.getLanguage().equals(selectedLanguage.getCode())) {
-                    question = Html.fromHtml(translation.getQuestionHtml()).toString();
+                    initWebView(translation.getQuestionHtml());
                 }
             }
         }
         if (question == null) {
-            question = Html.fromHtml(reviewQuestion.getQuestionHtml()).toString();
+            initWebView(reviewQuestion.getQuestionHtml());
         }
-        question = question.replaceAll("@font-face \\{.*\\}", "");
         updater.view.findViewById(R.id.marked_question).setVisibility(View.GONE);
         updater.view.findViewById(R.id.answered_question).setVisibility(View.GONE);
         updater.view.findViewById(R.id.all_question).setVisibility(View.VISIBLE);
         View indexView = updater.view.findViewById(R.id.question_index_all);
         setNumber(2, item.getIndex());
-        setText(0, question.trim());
 
         if (item.getResult() == null || item.getResult().equals(UNANSWERED)) {
             setBackgroundColor(indexView, R.color.testpress_text_gray);
@@ -73,8 +80,33 @@ class ReviewPanelListAdapter extends SingleTypeAdapter<ReviewItem> {
         }
         updater.view.findViewById(R.id.panel_item_layout).setBackgroundColor(
                 (position == currentItemPosition) ? Color.parseColor("#80b6dcfb") :
-                        Color.parseColor("#f9f9f9")
+                        Color.parseColor("#ffffff")
         );
+
+        updater.view.findViewById(R.id.question_all).setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction()==MotionEvent.ACTION_UP){
+                    listItemClickListener.onItemClicked(item.getIndex() - 1);
+                }
+                return false;
+            }
+        });
+
+        updater.view.findViewById(R.id.panel_item_layout).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listItemClickListener.onItemClicked(item.getIndex() - 1);
+            }
+        });
+    }
+
+    private void initWebView(String questionHtml) {
+        WebView questionAll = updater.view.findViewById(R.id.question_all);
+        String question = "<div style='padding-left: 6px; padding-right: 6px; padding-top: 0px;'>";
+        question = question + questionHtml;
+        WebViewUtils webViewUtils = new WebViewUtils(questionAll);
+        webViewUtils.initWebView(question, activity);
     }
 
     private void setBackgroundColor(View view, int colorResId) {
@@ -87,5 +119,9 @@ class ReviewPanelListAdapter extends SingleTypeAdapter<ReviewItem> {
 
     public void setSelectedLanguage(Language selectedLanguage) {
         this.selectedLanguage = selectedLanguage;
+    }
+
+    public interface ListItemClickListener {
+        void onItemClicked(int position);
     }
 }
--- exam/src/main/java/in/testpress/exam/ui/TestPanelListAdapter.java
+++ exam/src/main/java/in/testpress/exam/ui/TestPanelListAdapter.java
@@ -1,8 +1,10 @@
 package in.testpress.exam.ui;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.graphics.Color;
-import android.text.Html;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 
 import java.util.List;
@@ -10,8 +12,10 @@ import java.util.List;
 import in.testpress.exam.R;
 import in.testpress.exam.models.AttemptItem;
 import in.testpress.exam.models.AttemptQuestion;
+import in.testpress.exam.ui.view.WebView;
 import in.testpress.models.greendao.Language;
 import in.testpress.util.SingleTypeAdapter;
+import in.testpress.util.WebViewUtils;
 
 /**
  * Adapter that used to show the list of questions in test engine
@@ -20,9 +24,14 @@ class TestPanelListAdapter extends SingleTypeAdapter<AttemptItem> {
 
     private int currentAttemptItemIndex = 1;
     private Language selectedLanguage;
+    private final Activity activity;
+    private final ListItemClickListener listItemClickListener;
 
-    TestPanelListAdapter(final LayoutInflater inflater, final List<AttemptItem> items, int layout) {
+    TestPanelListAdapter(final LayoutInflater inflater, final List<AttemptItem> items,
+                         int layout, Activity activity, ListItemClickListener listItemClickListener) {
         super(inflater, layout);
+        this.activity = activity;
+        this.listItemClickListener = listItemClickListener;
         setItems(items);
     }
 
@@ -35,7 +44,9 @@ class TestPanelListAdapter extends SingleTypeAdapter<AttemptItem> {
     }
 
     @Override
+    @SuppressLint("ClickableViewAccessibility")
     protected void update(final int position, final AttemptItem item) {
+        WebView webView;
         AttemptQuestion attemptQuestion = item.getAttemptQuestion();
         List<AttemptQuestion> translations = attemptQuestion.getTranslations();
         if (translations.size() > 0 && selectedLanguage != null &&
@@ -47,36 +58,62 @@ class TestPanelListAdapter extends SingleTypeAdapter<AttemptItem> {
                 }
             }
         }
-        String question = Html.fromHtml(attemptQuestion.getQuestionHtml()).toString();
+        String question = attemptQuestion.getQuestionHtml();
         if(item.getReview() || item.getCurrentReview()) {
             // Marked question
+            webView = updater.view.findViewById(R.id.question_marked);
             updater.view.findViewById(R.id.all_question).setVisibility(View.GONE);
             updater.view.findViewById(R.id.answered_question).setVisibility(View.GONE);
             updater.view.findViewById(R.id.marked_question).setVisibility(View.VISIBLE);
             setNumber(3, item.getIndex());
-            setText(1, question.trim());
         } else if(!item.getSelectedAnswers().isEmpty() || !item.getSavedAnswers().isEmpty()
                 || (item.getShortText() != null && !item.getShortText().isEmpty())
                 || (item.getCurrentShortText() != null && !item.getCurrentShortText().isEmpty())) {
 
             // Answered question
+            webView = updater.view.findViewById(R.id.question_answered);
             updater.view.findViewById(R.id.marked_question).setVisibility(View.GONE);
             updater.view.findViewById(R.id.answered_question).setVisibility(View.VISIBLE);
             updater.view.findViewById(R.id.all_question).setVisibility(View.GONE);
             setNumber(5, item.getIndex());
-            setText(4, question.trim());
         } else {
             // Unanswered question
+            webView = updater.view.findViewById(R.id.question_all);
             updater.view.findViewById(R.id.marked_question).setVisibility(View.GONE);
             updater.view.findViewById(R.id.answered_question).setVisibility(View.GONE);
             updater.view.findViewById(R.id.all_question).setVisibility(View.VISIBLE);
             setNumber(2, item.getIndex());
-            setText(0, question.trim());
         }
         updater.view.findViewById(R.id.panel_item_layout).setBackgroundColor(
                 (item.getIndex() == currentAttemptItemIndex) ? Color.parseColor("#80b6dcfb") :
-                        Color.parseColor("#f9f9f9")
+                        Color.parseColor("#ffffff")
         );
+
+        webView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction()==MotionEvent.ACTION_UP){
+                    listItemClickListener.onItemClicked(item.getIndex() -1);
+                }
+                return false;
+            }
+        });
+
+        updater.view.findViewById(R.id.panel_item_layout).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listItemClickListener.onItemClicked(item.getIndex() - 1);
+            }
+        });
+
+        initWebView(webView, question);
+    }
+
+    private void initWebView(WebView view, String questionHtml) {
+        String question = "<div style='padding-left: 6px; padding-right: 6px; padding-top: 0px;'>";
+        question = question + questionHtml;
+        WebViewUtils webViewUtils = new WebViewUtils(view);
+        webViewUtils.initWebView(question, activity);
     }
 
     public void setCurrentAttemptItemIndex(int currentAttemptItemIndex) {
@@ -85,5 +122,9 @@ class TestPanelListAdapter extends SingleTypeAdapter<AttemptItem> {
 
     public void setSelectedLanguage(Language selectedLanguage) {
         this.selectedLanguage = selectedLanguage;
+    }
+
+    public interface ListItemClickListener {
+        void onItemClicked(int position);
     }
 }
