2e9cc26eea2c2d80eba06f6d68b6ba6db2dd2695
Sangeetha <sangeetha@testpress.in>
2021-01-04 18:52:55 +0530

    Refactor testcase

--- exam/src/test/java/in/testpress/exam/ui/SaveAttemptSectionTest.kt
+++ exam/src/test/java/in/testpress/exam/ui/SaveAttemptSectionTest.kt
@@ -8,25 +8,32 @@ import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
 import org.robolectric.RobolectricTestRunner
 
 @RunWith(RobolectricTestRunner::class)
 class SaveAttemptSectionTest {
 
-    private val attempt = Attempt()
+    private val attempt = Attempt(1)
 
-    private val courseAttempt = CourseAttempt()
+    var courseAttempt: CourseAttempt = Mockito.mock(CourseAttempt::class.java)
 
     private val context: Context = ApplicationProvider.getApplicationContext()
 
     private val attemptSectionDao =
             TestpressSDKDatabase.getAttemptSectionDao(context)
 
-    @Test
-    fun saveInDBShouldSaveAttemptSection() {
+    @Before
+    fun setup() {
+        MockitoAnnotations.initMocks(this)
+        Mockito.`when`(courseAttempt.rawAssessment).thenReturn(attempt)
         saveAttempt()
         saveAttemptSection()
+    }
 
+    @Test
+    fun saveInDBShouldSaveAttemptSection() {
         courseAttempt.saveInDB(context, Content(1))
 
         val section = attemptSectionDao.queryBuilder().where(AttemptSectionDao.Properties.AttemptId.eq(1)).list()
@@ -35,7 +42,7 @@ class SaveAttemptSectionTest {
 
     private fun saveAttemptSection() {
         val attemptSection = listOf(AttemptSection(
-                1,"state", "https://sandox.testpress.in","Start", "End",
+                1, "state", "https://sandox.testpress.in", "Start", "End",
                 "time", "name", "duration", 1,
                 "instruction", 1))
         attemptSectionDao.insertOrReplaceInTx(attemptSection)
@@ -44,8 +51,8 @@ class SaveAttemptSectionTest {
     private fun saveAttempt() {
         val attemptDao = TestpressSDKDatabase.getAttemptDao(ApplicationProvider.getApplicationContext())
         attemptDao.insertOrReplace(Attempt(
-                "https://sandox.testpress.in",1,"date",10,"10","2","10",
-                "reviewUrl","Question url",2,1,"","","","","",1,1,"per"
+                "https://sandox.testpress.in", 1, "date", 10, "10", "2", "10",
+                "reviewUrl", "Question url", 2, 1, "", "", "", "", "", 1, 1, "per"
         ))
     }
 }
