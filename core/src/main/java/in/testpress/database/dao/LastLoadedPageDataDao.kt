fca0fc7a3f4be9b01a0ae304834756860a36b538
Karthik <rajakarthik131@gmail.com>
2021-06-28 08:34:07 +0530

    Add Room DB integration

---
+++ core/src/main/java/in/testpress/database/dao/LastLoadedPageDataDao.kt
@@ -0,0 +1,15 @@
+package `in`.testpress.database.dao
+
+import `in`.testpress.database.BaseDao
+import `in`.testpress.database.entities.LastLoadedPageData
+import androidx.room.Dao
+import androidx.room.Query
+
+@Dao
+interface LastLoadedPageDataDao: BaseDao<LastLoadedPageData> {
+    @Query("SELECT * FROM LastLoadedPageData WHERE resourceType = :name")
+    suspend fun findPageDataForResource(name: String): LastLoadedPageData?
+
+    @Query("DELETE FROM LastLoadedPageData")
+    suspend fun deleteAll()
+}
