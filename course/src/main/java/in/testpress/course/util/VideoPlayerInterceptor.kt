46a2c0b53615505b82e65ceb961e45c138e5f0fc
PruthiviRaj27 <pruthiviraj@testpress.in>
2022-09-28 15:53:15 +0530

    Video Player Request to testpress domain url instead of manifest url

--- course/src/main/java/in/testpress/course/util/VideoPlayerInterceptor.kt
+++ course/src/main/java/in/testpress/course/util/VideoPlayerInterceptor.kt
@@ -4,16 +4,27 @@ import `in`.testpress.core.TestpressSdk
 import android.content.Context
 import okhttp3.Interceptor
 import okhttp3.Response
+import java.net.URI
 
-class VideoPlayerInterceptor (val context: Context) : Interceptor {
+class VideoPlayerInterceptor(val context: Context) : Interceptor {
 
     override fun intercept(chain: Interceptor.Chain): Response {
         var request = chain.request()
 
-        if(request.url.toString().contains("encryption_key")) {
+        if (request.url.toString().contains("encryption_key")) {
             val session = TestpressSdk.getTestpressSession(context)
-            request = request.newBuilder().addHeader("Authorization", "JWT " + session?.token).build()
+            val hostUrl = getHostUrl(context)
+            val updatedUrl = request.url.newBuilder().host(hostUrl).build()
+            request = request.newBuilder()
+                .addHeader("Authorization", "JWT " + session?.token)
+                .url(updatedUrl)
+                .build()
         }
         return chain.proceed(request)
     }
+}
+
+private fun getHostUrl(context: Context): String {
+    val uri = URI.create(TestpressSdk.getTestpressSession(context)?.instituteSettings?.baseUrl)
+    return uri.host
 }
