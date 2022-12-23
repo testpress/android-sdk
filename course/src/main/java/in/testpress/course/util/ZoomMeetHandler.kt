c6e6ba9bf495133de32089e819a2d5ae0660336d
Hari <hari@testpress.in>
2022-12-23 13:56:47 +0530

    refactor: Clean Zoom meeting callback handling code

--- course/src/main/java/in/testpress/course/util/ZoomMeetHandler.kt
+++ course/src/main/java/in/testpress/course/util/ZoomMeetHandler.kt
@@ -1,6 +1,7 @@
 package `in`.testpress.course.util
 
 import `in`.testpress.course.domain.DomainVideoConferenceContent
+import `in`.testpress.course.ui.callbacks.MeetingCommonCallback
 import `in`.testpress.models.ProfileDetails
 import `in`.testpress.util.isEmailValid
 import android.content.Context
@@ -13,8 +14,7 @@ class ZoomMeetHandler(
     val context: Context,
     val videoConference: DomainVideoConferenceContent,
     val profileDetails: ProfileDetails?
-) : MeetingServiceListener,
-    ZoomSDKInitializeListener, InMeetingServiceListener {
+) : ZoomSDKInitializeListener, MeetingCommonCallback.CommonEvent {
 
     private lateinit var zoomSDK: ZoomSDK
     private var onInitializeCallback: VideoConferenceInitializeListener? = null
@@ -68,9 +68,11 @@ class ZoomMeetHandler(
     }
 
     private fun registerMeetingServiceListener() {
-        val meetingService = zoomSDK.meetingService
-        meetingService?.addListener(this)
-        zoomSDK.inMeetingService.addListener(this)
+        MeetingCommonCallback.addListener(this)
+    }
+
+    override fun onMeetingFail(errorCode: Int, internalErrorCode: Int) {
+
     }
 
     override fun onMeetingStatusChanged(
@@ -91,8 +93,14 @@ class ZoomMeetHandler(
         }
     }
 
-    override fun onMeetingParameterNotification(p0: MeetingParameter?) {
+    override fun onMeetingNeedCloseOtherMeeting(inMeetingEventHandler: InMeetingEventHandler) {
+    }
 
+    override fun onMeetingNeedPasswordOrDisplayName(
+        needPassword: Boolean,
+        needUsername: Boolean,
+        inMeetingEventHandler: InMeetingEventHandler
+    ) {
     }
 
     override fun onZoomSDKInitializeResult(errorCode: Int, internalErrorCode: Int) {
@@ -121,8 +129,7 @@ class ZoomMeetHandler(
 
     fun removeListeners() {
         if (zoomSDK.isInitialized) {
-            val meetingService = zoomSDK.meetingService
-            meetingService.removeListener(this)
+            MeetingCommonCallback.removeListener(this)
         }
     }
 
@@ -157,185 +164,10 @@ class ZoomMeetHandler(
         return params
     }
 
-    override fun onMeetingActiveVideo(p0: Long) {
-    }
-
-    override fun onFreeMeetingReminder(p0: Boolean, p1: Boolean, p2: Boolean) {
-    }
-
     override fun onJoinWebinarNeedUserNameAndEmail(inMeetingEventHandler: InMeetingEventHandler) {
         if (profileDetails != null && profileDetails.email.isEmailValid()) {
             val name = profileDetails.displayName ?: profileDetails.username
             inMeetingEventHandler.setRegisterWebinarInfo(name, profileDetails.email, false)
         }
-    }
-
-    override fun onMeetingNeedCloseOtherMeeting(p0: InMeetingEventHandler?) {
-        
-    }
-
-    override fun onActiveVideoUserChanged(p0: Long) {
-    }
-
-    override fun onActiveSpeakerVideoUserChanged(p0: Long) {
-    }
-
-    override fun onHostVideoOrderUpdated(p0: MutableList<Long>?) {
-
-    }
-
-    override fun onFollowHostVideoOrderChanged(p0: Boolean) {
-        
-    }
-
-    override fun onChatMessageReceived(p0: InMeetingChatMessage?) {
-    }
-
-    override fun onChatMsgDeleteNotification(p0: String?, p1: ChatMessageDeleteType?) {
-
-    }
-
-
-    override fun onUserNetworkQualityChanged(p0: Long) {
-    }
-
-    override fun onSinkMeetingVideoQualityChanged(p0: VideoQuality?, p1: Long) {
-
-    }
-
-
-    override fun onMeetingUserJoin(p0: MutableList<Long>?) {
-    }
-
-    override fun onRecordingStatus(p0: InMeetingServiceListener.RecordingStatus?) {
-         
-    }
-
-    override fun onLocalRecordingStatus(p0: Long, p1: InMeetingServiceListener.RecordingStatus?) {
-
-    }
-
-    override fun onInvalidReclaimHostkey() {
-        
-    }
-
-    override fun onPermissionRequested(p0: Array<out String>?) {
-
-    }
-
-    override fun onAllHandsLowered() {
-
-    }
-
-    override fun onLocalVideoOrderUpdated(p0: MutableList<Long>?) {
-
-    }
-
-    override fun onMeetingUserLeave(p0: MutableList<Long>?) {
-    }
-
-    override fun onMeetingFail(p0: Int, p1: Int) {
-    }
-
-    override fun onFreeMeetingUpgradeToProMeeting() {
-         
-    }
-
-    override fun onClosedCaptionReceived(p0: String?, p1: Long) {
-
-    }
-
-    override fun onFreeMeetingNeedToUpgrade(p0: FreeMeetingNeedUpgradeType?, p1: String?) {
-         
-    }
-
-    override fun onUserAudioTypeChanged(p0: Long) {
-    }
-
-    override fun onFreeMeetingUpgradeToGiftFreeTrialStop() {
-         
-    }
-
-    override fun onMyAudioSourceTypeChanged(p0: Int) {
-    }
-
-    override fun onSilentModeChanged(p0: Boolean) {
-    }
-
-    override fun onFreeMeetingUpgradeToGiftFreeTrialStart() {
-         
-    }
-
-    override fun onMeetingCoHostChanged(p0: Long) {
-    }
-
-    override fun onMeetingCoHostChange(p0: Long, p1: Boolean) {
-
-    }
-
-    override fun onLowOrRaiseHandStatusChanged(p0: Long, p1: Boolean) {
-    }
-
-    override fun onSinkAttendeeChatPriviledgeChanged(p0: Int) {
-    }
-
-    override fun onMeetingUserUpdated(p0: Long) {
-    }
-
-    override fun onMicrophoneStatusError(p0: InMeetingAudioController.MobileRTCMicrophoneError?) {
-    }
-
-    override fun onHostAskStartVideo(p0: Long) {
-    }
-
-    override fun onSinkAllowAttendeeChatNotification(p0: Int) {
-    }
-
-    override fun onSinkPanelistChatPrivilegeChanged(p0: InMeetingChatController.MobileRTCWebinarPanelistChatPrivilege?) {
-
-    }
-
-
-    override fun onSpotlightVideoChanged(p0: Boolean) {
-    }
-
-    override fun onSpotlightVideoChanged(p0: MutableList<Long>?) {
-
-    }
-
-
-    override fun onMeetingHostChanged(p0: Long) {
-    }
-
-    override fun onMeetingLeaveComplete(p0: Long) {
-    }
-
-    override fun onHostAskUnMute(p0: Long) {
-    }
-
-    override fun onUserAudioStatusChanged(p0: Long, p1: InMeetingServiceListener.AudioStatus?) {
-         
-    }
-
-    override fun onUserNameChanged(p0: Long, p1: String?) {
-    }
-
-    override fun onUserNamesChanged(p0: MutableList<Long>?) {
-
-    }
-
-    override fun onMeetingNeedPasswordOrDisplayName(
-        p0: Boolean,
-        p1: Boolean,
-        p2: InMeetingEventHandler?
-    ) {
-    }
-
-    override fun onWebinarNeedRegister(p0: String?) {
-        
-    }
-
-    override fun onUserVideoStatusChanged(p0: Long, p1: InMeetingServiceListener.VideoStatus?) {
-         
     }
 }
