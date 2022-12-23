ca3698b13a8ce96784f7d4af4232654d62381f29
Hari <hari@testpress.in>
2022-12-23 18:32:42 +0530

    refactor: Extract zoom meeting callbacks (#462)

---
+++ course/src/main/java/in/testpress/course/domain/zoom/callbacks/BaseCallback.kt
@@ -0,0 +1,18 @@
+package `in`.testpress.course.ui.callbacks
+
+
+open class BaseCallback<T : BaseEvent?> {
+    protected var callbacks: MutableList<T> = ArrayList()
+
+    fun addListener(event: T) {
+        if (!callbacks.contains(event)) {
+            callbacks.add(event)
+        }
+    }
+
+    fun removeListener(event: T) {
+        callbacks.remove(event)
+    }
+}
+
+interface BaseEvent
---
+++ course/src/main/java/in/testpress/course/util/SimpleInMeetingListener.kt
@@ -0,0 +1,86 @@
+package `in`.testpress.course.util
+
+
+import us.zoom.sdk.ChatMessageDeleteType
+import us.zoom.sdk.FreeMeetingNeedUpgradeType
+import us.zoom.sdk.InMeetingAudioController.MobileRTCMicrophoneError
+import us.zoom.sdk.InMeetingChatController.MobileRTCWebinarPanelistChatPrivilege
+import us.zoom.sdk.InMeetingChatMessage
+import us.zoom.sdk.InMeetingEventHandler
+import us.zoom.sdk.InMeetingServiceListener
+import us.zoom.sdk.InMeetingServiceListener.RecordingStatus
+import us.zoom.sdk.VideoQuality
+
+open class SimpleInMeetingListener : InMeetingServiceListener {
+    override fun onMeetingNeedPasswordOrDisplayName(
+        b: Boolean,
+        b1: Boolean,
+        inMeetingEventHandler: InMeetingEventHandler
+    ) {
+    }
+
+    override fun onWebinarNeedRegister(registerUrl: String) {}
+    override fun onJoinWebinarNeedUserNameAndEmail(inMeetingEventHandler: InMeetingEventHandler) {}
+    override fun onMeetingNeedCloseOtherMeeting(inMeetingEventHandler: InMeetingEventHandler) {}
+    override fun onMeetingFail(i: Int, i1: Int) {}
+    override fun onMeetingLeaveComplete(l: Long) {}
+    override fun onMeetingUserJoin(list: List<Long>) {}
+    override fun onMeetingUserLeave(list: List<Long>) {}
+    override fun onMeetingUserUpdated(l: Long) {}
+    override fun onMeetingHostChanged(l: Long) {}
+    override fun onMeetingCoHostChanged(l: Long) {}
+    override fun onMeetingCoHostChange(userId: Long, isCoHost: Boolean) {}
+    override fun onActiveVideoUserChanged(var1: Long) {}
+    override fun onActiveSpeakerVideoUserChanged(var1: Long) {}
+    override fun onSpotlightVideoChanged(b: Boolean) {}
+    override fun onSpotlightVideoChanged(userList: List<Long>) {}
+    override fun onUserVideoStatusChanged(
+        userId: Long,
+        status: InMeetingServiceListener.VideoStatus
+    ) {
+    }
+
+    override fun onMicrophoneStatusError(mobileRTCMicrophoneError: MobileRTCMicrophoneError) {}
+    override fun onUserAudioStatusChanged(
+        userId: Long,
+        audioStatus: InMeetingServiceListener.AudioStatus
+    ) {
+    }
+
+    override fun onUserAudioTypeChanged(l: Long) {}
+    override fun onMyAudioSourceTypeChanged(i: Int) {}
+    override fun onLowOrRaiseHandStatusChanged(l: Long, b: Boolean) {}
+    override fun onChatMessageReceived(inMeetingChatMessage: InMeetingChatMessage) {}
+    override fun onChatMsgDeleteNotification(msgID: String, deleteBy: ChatMessageDeleteType) {}
+    override fun onUserNetworkQualityChanged(userId: Long) {}
+    override fun onSinkMeetingVideoQualityChanged(videoQuality: VideoQuality, userId: Long) {}
+    override fun onHostAskUnMute(userId: Long) {}
+    override fun onHostAskStartVideo(userId: Long) {}
+    override fun onSilentModeChanged(inSilentMode: Boolean) {}
+    override fun onFreeMeetingReminder(
+        isOrignalHost: Boolean,
+        canUpgrade: Boolean,
+        isFirstGift: Boolean
+    ) {
+    }
+
+    override fun onMeetingActiveVideo(userId: Long) {}
+    override fun onSinkAttendeeChatPriviledgeChanged(privilege: Int) {}
+    override fun onSinkAllowAttendeeChatNotification(privilege: Int) {}
+    override fun onSinkPanelistChatPrivilegeChanged(privilege: MobileRTCWebinarPanelistChatPrivilege) {}
+    override fun onUserNameChanged(userId: Long, name: String) {}
+    override fun onUserNamesChanged(userList: List<Long>) {}
+    override fun onFreeMeetingNeedToUpgrade(type: FreeMeetingNeedUpgradeType, gifUrl: String) {}
+    override fun onFreeMeetingUpgradeToGiftFreeTrialStart() {}
+    override fun onFreeMeetingUpgradeToGiftFreeTrialStop() {}
+    override fun onFreeMeetingUpgradeToProMeeting() {}
+    override fun onClosedCaptionReceived(message: String, senderId: Long) {}
+    override fun onRecordingStatus(status: RecordingStatus) {}
+    override fun onLocalRecordingStatus(userId: Long, status: RecordingStatus) {}
+    override fun onInvalidReclaimHostkey() {}
+    override fun onHostVideoOrderUpdated(orderList: List<Long>) {}
+    override fun onFollowHostVideoOrderChanged(bFollow: Boolean) {}
+    override fun onPermissionRequested(permissions: Array<String>) {}
+    override fun onAllHandsLowered() {}
+    override fun onLocalVideoOrderUpdated(localOrderList: List<Long>) {}
+}
