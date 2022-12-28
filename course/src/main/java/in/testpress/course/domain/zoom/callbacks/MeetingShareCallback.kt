54ea8f4a25b8a2708702c2eab71e609fdbbc7d9e
Hari <hari@testpress.in>
2022-12-28 07:49:30 +0530

    feat: Open zoom meeting horizontally on start (#463)
    - Enabled zoom customized meeting UI.
    - Opened zoom meeting horizontally after the user clicks on the start button.
    - Allowed user to provide username on meeting join.

---
+++ course/src/main/java/in/testpress/course/domain/zoom/callbacks/MeetingShareCallback.kt
@@ -0,0 +1,29 @@
+package `in`.testpress.course.domain.zoom.callbacks
+
+import us.zoom.sdk.InMeetingShareController.InMeetingShareListener
+import us.zoom.sdk.ShareSettingType
+import us.zoom.sdk.SharingStatus
+import us.zoom.sdk.ZoomSDK
+
+
+object MeetingShareCallback: BaseCallback<MeetingShareCallback.ShareEvent?>() {
+    interface ShareEvent : BaseEvent {
+        fun onSharingStatus(status: SharingStatus, userId: Long)
+    }
+
+    private var shareListener: InMeetingShareListener = object : InMeetingShareListener {
+        override fun onSharingStatus(status: SharingStatus, userId: Long) {
+            for (event in callbacks) {
+                event?.onSharingStatus(status, userId)
+            }
+        }
+
+        override fun onShareActiveUser(p0: Long) {}
+        override fun onShareUserReceivingStatus(p0: Long) {}
+        override fun onShareSettingTypeChanged(p0: ShareSettingType?) {}
+    }
+
+    init{
+        ZoomSDK.getInstance().inMeetingService.inMeetingShareController.addListener(shareListener)
+    }
+}
---
+++ course/src/main/java/in/testpress/course/ui/MeetingScreenFragment.kt
@@ -0,0 +1,69 @@
+package `in`.testpress.course.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import `in`.testpress.course.databinding.MeetingScreenBinding
+import `in`.testpress.course.domain.zoom.callbacks.MeetingShareCallback
+import us.zoom.sdk.*
+
+
+class MeetingScreenFragment : Fragment() , MeetingShareCallback.ShareEvent {
+    private lateinit var meetingScreenBinding: MeetingScreenBinding
+    private lateinit var inMeetingService: InMeetingService
+    private lateinit var primaryVideoViewManager: MobileRTCVideoViewManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        inMeetingService = ZoomSDK.getInstance().inMeetingService
+        MeetingShareCallback.addListener(this)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        meetingScreenBinding = MeetingScreenBinding.inflate(inflater, container, false)
+        return meetingScreenBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        primaryVideoViewManager = meetingScreenBinding.primaryMeetingView.videoViewManager
+        renderVideo()
+    }
+
+    override fun onSharingStatus(status: SharingStatus, userId: Long) {
+        if (inMeetingService.isHostUser(userId) &&
+            status == SharingStatus.Sharing_Other_Share_Begin ||
+            status == SharingStatus.Sharing_Other_Share_End){
+            renderVideo()
+        }
+    }
+
+    private fun renderVideo() {
+        primaryVideoViewManager.removeAllVideoUnits()
+
+        val defaultVideoViewRenderInfo = MobileRTCVideoUnitRenderInfo(0, 0, 100, 100)
+        val screenShareUserId = inMeetingService.activeShareUserID()
+        if (inMeetingService.inMeetingShareController.isOtherSharing && inMeetingService.isHostUser(screenShareUserId)){
+            primaryVideoViewManager.addShareVideoUnit(screenShareUserId, defaultVideoViewRenderInfo)
+        } else {
+            primaryVideoViewManager.addActiveVideoUnit(defaultVideoViewRenderInfo)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        meetingScreenBinding.primaryMeetingView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        meetingScreenBinding.primaryMeetingView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        primaryVideoViewManager.removeAllVideoUnits()
+    }
+}
