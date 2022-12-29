e58f78c5f7d973bc32042c81e72d0d6469b0e2d7
Hari <hari@testpress.in>
2022-12-29 17:20:26 +0530

    Refactor the code

--- course/src/main/java/in/testpress/course/ui/MessageListAdapter.kt
+++ course/src/main/java/in/testpress/course/ui/MessageListAdapter.kt
@@ -1,51 +1,37 @@
 package `in`.testpress.course.ui;
 
 import `in`.testpress.course.R
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import us.zoom.sdk.InMeetingChatMessage
 
 
 class MessageListAdapter(private val currentUserId: Long) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    private val VIEW_TYPE_MESSAGE_SENT = 1
-    private val VIEW_TYPE_MESSAGE_RECEIVED = 2
     private var messages = mutableListOf<InMeetingChatMessage>()
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return if (viewType == VIEW_TYPE_MESSAGE_SENT) {
-            SentMessageHolder(LayoutInflater.from(parent.context)
-                .inflate(R.layout.my_message, parent, false))
-        } else {
-            ReceivedMessageHolder(LayoutInflater.from(parent.context)
-                .inflate(R.layout.received_message, parent, false))
-        }
+        return MessageHolder(LayoutInflater.from(parent.context)
+                .inflate(R.layout.message, parent, false))
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val message = messages[position]
         val previousMessage = messages.getOrNull(position - 1)
 
-        when (holder.itemViewType) {
-            VIEW_TYPE_MESSAGE_SENT -> (holder as SentMessageHolder).bind(message)
-            VIEW_TYPE_MESSAGE_RECEIVED -> (holder as ReceivedMessageHolder).bind(message, previousMessage)
-        }
+        (holder as MessageHolder).bind(message, previousMessage, isCurrentUserMessage(message))
     }
+
+    private fun isCurrentUserMessage(message: InMeetingChatMessage) = run { currentUserId == message.senderUserId }
 
     override fun getItemCount() = messages.count()
 
     override fun getItemId(position: Int) = position.toLong()
-
-    override fun getItemViewType(position: Int): Int {
-        return if (messages[position].senderUserId == currentUserId) {
-            VIEW_TYPE_MESSAGE_SENT
-        } else {
-            VIEW_TYPE_MESSAGE_RECEIVED
-        }
-    }
 
     fun addMessage(message: InMeetingChatMessage?){
         if (message != null) {
@@ -55,35 +41,31 @@ class MessageListAdapter(private val currentUserId: Long) : RecyclerView.Adapter
     }
 
 
-    private class ReceivedMessageHolder constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    private class MessageHolder constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var messageCard: CardView
         var messageText: TextView
         var nameText: TextView
 
         init {
             nameText = itemView.findViewById(R.id.sender)
             messageText = itemView.findViewById(R.id.message)
+            messageCard = itemView.findViewById(R.id.message_card)
         }
 
-        fun bind(message: InMeetingChatMessage, previousMessage: InMeetingChatMessage?) {
+        fun bind(message: InMeetingChatMessage, previousMessage: InMeetingChatMessage?, isCurrentUserMessage: Boolean) {
             messageText.text = message.content
-            nameText.text = message.senderDisplayName
-            if(previousMessage?.senderUserId == message.senderUserId){
-                nameText.visibility = View.GONE
-            }else{
-                nameText.visibility = View.VISIBLE
+            nameText.text = if (isCurrentUserMessage) "Me" else message.senderDisplayName
+            nameText.visibility = if (previousMessage?.senderUserId == message.senderUserId) View.GONE else View.VISIBLE
+            setColorOnMessageCard(isCurrentUserMessage)
             }
-        }
-    }
 
-    private class SentMessageHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var messageText: TextView
-
-        init {
-            messageText = itemView.findViewById(R.id.my_chat_message)
-        }
-
-        fun bind(message: InMeetingChatMessage) {
-            messageText.text = message.content
+        fun setColorOnMessageCard(isCurrentUserMessage: Boolean){
+            val color = if (isCurrentUserMessage){
+                Color.parseColor("#dff0ff")
+            }else{
+                Color.parseColor("#eef1f6")
+            }
+            messageCard.setCardBackgroundColor(color)
         }
     }
 }
