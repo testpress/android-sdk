2a24b8b1f002d5b9b372d5681d0f589030160e41
PruthiviRaj27 <pruthiviraj@testpress.in>
2023-01-10 13:32:39 +0530

    fix:Hide zoom share link

--- course/src/main/java/in/testpress/course/util/ZoomMeetHandler.kt
+++ course/src/main/java/in/testpress/course/util/ZoomMeetHandler.kt
@@ -13,8 +13,7 @@ import android.content.Context
 import android.content.Intent
 import android.widget.Toast
 import us.zoom.sdk.*
-import us.zoom.sdk.MeetingViewsOptions.NO_TEXT_MEETING_ID
-import us.zoom.sdk.MeetingViewsOptions.NO_TEXT_PASSWORD
+import us.zoom.sdk.MeetingViewsOptions.*
 
 class ZoomMeetHandler(
     val context: Context,
@@ -170,6 +169,7 @@ class ZoomMeetHandler(
     }
 
     fun startMeeting() {
+        zoomSDK.zoomUIService.hideMeetingInviteUrl(true)
         val meetingService = zoomSDK.meetingService
         val ret = meetingService.joinMeetingWithParams(
             context,
