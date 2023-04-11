16a45513f9bd8f4aec9b3fea033258b90af32779
PruthiviRaj27 <pruthiviraj@testpress.in>
2023-04-11 12:17:37 +0530

    refcator

--- course/src/main/java/in/testpress/course/ui/CourseDetailsTabAdapter.java
+++ course/src/main/java/in/testpress/course/ui/CourseDetailsTabAdapter.java
@@ -1,57 +1,36 @@
 package in.testpress.course.ui;
 
-import android.content.res.Resources;
-import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
-import in.testpress.course.R;
+import java.util.LinkedHashMap;
 
 class CourseDetailsTabAdapter extends FragmentPagerAdapter {
 
-    private Resources resources;
-    private Bundle bundle;
+    private LinkedHashMap<Fragment, String> fragmentList;
 
-    CourseDetailsTabAdapter(Resources resources, FragmentManager fragmentManager, Bundle bundle) {
+    CourseDetailsTabAdapter(
+            FragmentManager fragmentManager,
+            LinkedHashMap<Fragment, String> fragmentList
+    ) {
         super(fragmentManager);
-        this.resources = resources;
-        this.bundle = bundle;
+        this.fragmentList = fragmentList;
     }
 
     @Override
     public int getCount() {
-        return 2;
+        return fragmentList.size();
     }
 
     @Override
     public Fragment getItem(int position) {
-        Fragment fragment;
-        switch (position) {
-            case 0:
-                fragment = new ChaptersListFragment();
-                break;
-            case 1:
-                fragment = new RankListFragment();
-                break;
-            default:
-                fragment = new ChaptersListFragment();
-                break;
-        }
-        fragment.setArguments(bundle);
-        return fragment;
+        return (Fragment) fragmentList.keySet().toArray()[position];
     }
 
     @Override
     public CharSequence getPageTitle(final int position) {
-        switch (position) {
-            case 0:
-                return resources.getString(R.string.testpress_learn);
-            case 1:
-                return resources.getString(R.string.testpress_leaderboard);
-            default:
-                return null;
-        }
+        return fragmentList.get(getItem(position));
     }
 
 }
