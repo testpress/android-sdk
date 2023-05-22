1420b0c4324d019f23f07217679c5a335f02fb88
PruthiviRaj27 <112844543+PruthiviRaj27@users.noreply.github.com>
2023-05-22 13:46:33 +0530

    refactor: BaseToolBarActivity Java to kotlin
    -  BaseToolBarActivity, BaseToolBarActivityTest
    - Added constant value to handle null pointer exception

--- core/src/main/java/in/testpress/Constants.kt
+++ core/src/main/java/in/testpress/Constants.kt
@@ -3,3 +3,10 @@ package `in`.testpress
 object WebViewConstants {
     const val REQUEST_SELECT_FILE = 1001
 }
+
+object Constants {
+
+    const val DEFAULT_ATTACHMENT_TITLE = "Attachment"
+    const val DEFAULT_ATTEMPTS_TITLE = "Attempts"
+
+}
