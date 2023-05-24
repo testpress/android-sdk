62a8b1114b21cd91d424b32f879a4817c3729c46
PruthiviRaj27 <112844543+PruthiviRaj27@users.noreply.github.com>
2023-05-24 16:46:39 +0530

    fix: Not able to Download Attachments in SDK version 30 above (#527)
    - For Android S+ (version 31) and above, it is recommended to include
    the FLAG_IMMUTABLE flag when creating a PendingIntent.
    - Reference -
    [link](https://developer.android.com/topic/security/risks/pending-intent#flag_immutable)

--- core/src/main/java/in/testpress/util/FileDownloadBroadcastReceiver.kt
+++ core/src/main/java/in/testpress/util/FileDownloadBroadcastReceiver.kt
@@ -75,7 +75,7 @@ class FileDownloaderBroadcastReceiver: BroadcastReceiver() {
             context,
             0,
             intent,
-            PendingIntent.FLAG_UPDATE_CURRENT
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
         )
     }
 
