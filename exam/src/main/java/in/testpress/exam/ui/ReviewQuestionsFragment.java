9f6d64938bc2d873426d4caa8e9b9b595f501d81
PruthiviRaj27 <pruthiviraj@testpress.in>
2023-06-05 19:12:28 +0530

    handle exam object

--- exam/src/main/java/in/testpress/exam/ui/ReviewQuestionsFragment.java
+++ exam/src/main/java/in/testpress/exam/ui/ReviewQuestionsFragment.java
@@ -373,12 +373,14 @@ public class ReviewQuestionsFragment extends Fragment {
                 "</div>";
 
         // Add Report button
-        html += "<div class='report-button' style='float:right;' onclick='onClickReportButton()'>" +
-                "<svg xmlns=\"http://www.w3.org/2000/svg\" fill=\"none\" viewBox=\"0 0 24 24\" stroke-width=\"1.5\" stroke=\"currentColor\" style=\"width:1rem;height:1rem;vertical-align: sub;\">\n" +
-                "  <path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z\" />\n" +
-                "</svg>\n" +
-                "Report Question" +
-                "</div>";
+        if (examId != -1L){
+            html += "<div class='report-button' style='float:right;' onclick='onClickReportButton()'>" +
+                    "<svg xmlns=\"http://www.w3.org/2000/svg\" fill=\"none\" viewBox=\"0 0 24 24\" stroke-width=\"1.5\" stroke=\"currentColor\" style=\"width:1rem;height:1rem;vertical-align: sub;\">\n" +
+                    "  <path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z\" />\n" +
+                    "</svg>\n" +
+                    "Report Question" +
+                    "</div>";
+        }
 
 
         // Add direction/passage
