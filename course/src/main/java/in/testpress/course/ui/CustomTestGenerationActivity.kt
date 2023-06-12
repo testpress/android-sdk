524cfd52a27fcef4466974424a61d6d6e321c66e
PruthiviRaj27 <pruthiviraj@testpress.in>
2023-06-12 15:53:56 +0530

    refcator

--- course/src/main/java/in/testpress/course/ui/CustomTestGenerationActivity.kt
+++ course/src/main/java/in/testpress/course/ui/CustomTestGenerationActivity.kt
@@ -99,26 +99,30 @@ class JavaScriptInterface(val activity: Activity):BaseJavaScriptInterface(activi
     }
 
     private fun parseJsonToAttempt(json: String): Attempt {
-        val jsonObject = JSONObject(json)
-        val attempt = Attempt()
-        attempt.url = jsonObject.optString("url")
-        attempt.id = jsonObject.optLong("id")
-        attempt.date = jsonObject.optString("date")
-        attempt.totalQuestions = jsonObject.optInt("total_questions")
-        attempt.score = jsonObject.optString("score")
-        attempt.reviewUrl = jsonObject.optString("review_url")
-        attempt.questionsUrl = jsonObject.optString("questions_url")
-        attempt.correctCount = jsonObject.optInt("correct_count")
-        attempt.incorrectCount = jsonObject.optInt("incorrect_count")
-        attempt.lastStartedTime = jsonObject.optString("last_started_time")
-        attempt.remainingTime = jsonObject.optString("remaining_time")
-        attempt.timeTaken = jsonObject.optString("time_taken")
-        attempt.state = jsonObject.optString("state")
-        attempt.speed = jsonObject.optInt("speed")
-        attempt.accuracy = jsonObject.optInt("accuracy")
-        attempt.percentage = jsonObject.optString("percentage")
-        attempt.lastViewedQuestionId = jsonObject.optInt("last_viewed_question_id")
-        attempt.reviewPdf = jsonObject.optString("review_pdf")
-        return attempt
+        try {
+            val jsonObject = JSONObject(json)
+            val attempt = Attempt()
+            attempt.url = jsonObject.optString("url")
+            attempt.id = jsonObject.optLong("id")
+            attempt.date = jsonObject.optString("date")
+            attempt.totalQuestions = jsonObject.optInt("total_questions")
+            attempt.score = jsonObject.optString("score")
+            attempt.reviewUrl = jsonObject.optString("review_url")
+            attempt.questionsUrl = jsonObject.optString("questions_url")
+            attempt.correctCount = jsonObject.optInt("correct_count")
+            attempt.incorrectCount = jsonObject.optInt("incorrect_count")
+            attempt.lastStartedTime = jsonObject.optString("last_started_time")
+            attempt.remainingTime = jsonObject.optString("remaining_time")
+            attempt.timeTaken = jsonObject.optString("time_taken")
+            attempt.state = jsonObject.optString("state")
+            attempt.speed = jsonObject.optInt("speed")
+            attempt.accuracy = jsonObject.optInt("accuracy")
+            attempt.percentage = jsonObject.optString("percentage")
+            attempt.lastViewedQuestionId = jsonObject.optInt("last_viewed_question_id")
+            attempt.reviewPdf = jsonObject.optString("review_pdf")
+            return attempt
+        } catch (e: JSONException) {
+            throw e
+        }
     }
 }
