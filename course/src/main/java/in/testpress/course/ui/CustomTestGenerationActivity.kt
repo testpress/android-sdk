d1ad59bdef568f73586ef21c901419309dbab63c
PruthiviRaj27 <112844543+PruthiviRaj27@users.noreply.github.com>
2023-06-13 11:44:29 +0530

    feat: Add Custom test generation (#544)
    - In this commit, we added a custom test generation feature and also
    added a button in the sample app to access this feature by providing a
    course slug as input

---
+++ course/src/main/java/in/testpress/course/ui/CustomTestGenerationActivity.kt
@@ -0,0 +1,63 @@
+package `in`.testpress.course.ui
+
+import `in`.testpress.exam.ui.ReviewStatsActivity
+import `in`.testpress.models.greendao.Attempt
+import `in`.testpress.ui.AbstractWebViewActivity
+import `in`.testpress.util.BaseJavaScriptInterface
+import android.app.Activity
+import android.webkit.JavascriptInterface
+import android.widget.Toast
+import org.json.JSONException
+import org.json.JSONObject
+
+class CustomTestGenerationActivity: AbstractWebViewActivity() {
+
+    override fun onWebViewInitializationSuccess() {
+        webViewFragment.addJavascriptInterface(JavaScriptInterface(this),"AndroidInterface")
+    }
+
+}
+
+class JavaScriptInterface(val activity: Activity):BaseJavaScriptInterface(activity) {
+
+    @JavascriptInterface
+    fun onExamEndCallBack(jsonData: String) {
+        try {
+            val attempt = parseJsonToAttempt(jsonData)
+            activity.finish()
+            activity.startActivity(ReviewStatsActivity.createIntent(activity, attempt))
+        } catch (e: JSONException) {
+            activity.finish()
+            Toast.makeText(activity, "Review Not available for this exam", Toast.LENGTH_SHORT)
+                .show()
+        }
+    }
+
+    private fun parseJsonToAttempt(json: String): Attempt {
+        try {
+            val jsonObject = JSONObject(json)
+            val attempt = Attempt()
+            attempt.url = jsonObject.optString("url")
+            attempt.id = jsonObject.optLong("id")
+            attempt.date = jsonObject.optString("date")
+            attempt.totalQuestions = jsonObject.optInt("total_questions")
+            attempt.score = jsonObject.optString("score")
+            attempt.reviewUrl = jsonObject.optString("review_url")
+            attempt.questionsUrl = jsonObject.optString("questions_url")
+            attempt.correctCount = jsonObject.optInt("correct_count")
+            attempt.incorrectCount = jsonObject.optInt("incorrect_count")
+            attempt.lastStartedTime = jsonObject.optString("last_started_time")
+            attempt.remainingTime = jsonObject.optString("remaining_time")
+            attempt.timeTaken = jsonObject.optString("time_taken")
+            attempt.state = jsonObject.optString("state")
+            attempt.speed = jsonObject.optInt("speed")
+            attempt.accuracy = jsonObject.optInt("accuracy")
+            attempt.percentage = jsonObject.optString("percentage")
+            attempt.lastViewedQuestionId = jsonObject.optInt("last_viewed_question_id")
+            attempt.reviewPdf = jsonObject.optString("review_pdf")
+            return attempt
+        } catch (e: JSONException) {
+            throw e
+        }
+    }
+}
