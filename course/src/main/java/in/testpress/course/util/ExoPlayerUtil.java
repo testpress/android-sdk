b78dbee98d2eae302725459123074a5692656e59
PruthiviRaj27 <pruthiviraj@testpress.in>
2023-06-20 14:30:23 +0530

    fix: Not able to swith Auto resolution in video

--- course/src/main/java/in/testpress/course/util/ExoPlayerUtil.java
+++ course/src/main/java/in/testpress/course/util/ExoPlayerUtil.java
@@ -398,7 +398,10 @@ public class ExoPlayerUtil implements VideoTimeRangeListener, DrmSessionManagerP
                     MappingTrackSelector.MappedTrackInfo mappedTrackInfo = trackSelector.getCurrentMappedTrackInfo();
                     int rendererIndex = getRendererIndex(C.TRACK_TYPE_VIDEO, mappedTrackInfo);
                     DefaultTrackSelector.ParametersBuilder parametersBuilder = trackSelector.buildUponParameters();
-                    if (!trackSelectionDialog.getOverrides().isEmpty()) {
+                    if (trackSelectionDialog.getOverrides().isEmpty()) {
+                        parametersBuilder.clearSelectionOverrides(rendererIndex);
+                        trackSelector.setParameters(parametersBuilder.build());
+                    } else {
                         parametersBuilder.clearSelectionOverrides(rendererIndex)
                                 .setSelectionOverride(rendererIndex, mappedTrackInfo.getTrackGroups(rendererIndex), trackSelectionDialog.getOverrides().get(0));
                         trackSelector.setParameters(parametersBuilder.build());
@@ -798,6 +801,8 @@ public class ExoPlayerUtil implements VideoTimeRangeListener, DrmSessionManagerP
             displayError(R.string.license_error);
         }
     }
+
+    DefaultTrackSelector.SelectionOverride override;
 
     private void playLowBitrateTrack() {
         MappingTrackSelector.MappedTrackInfo mappedTrackInfo = trackSelector.getCurrentMappedTrackInfo();
