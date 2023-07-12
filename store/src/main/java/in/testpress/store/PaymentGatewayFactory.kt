b9d415fe5b956b3dee0e5809c91c322616c4b9ad
Shantanu Tomar <shantanu@testpress.in>
2023-07-12 19:12:13 +0530

    Refactor code

--- store/src/main/java/in/testpress/store/PaymentGatewayFactory.kt
+++ store/src/main/java/in/testpress/store/PaymentGatewayFactory.kt
@@ -6,14 +6,11 @@ import android.app.Activity
 import `in`.testpress.store.razorpay.RazorpayPaymentGateway
 import `in`.testpress.core.TestpressSdk
 import `in`.testpress.models.InstituteSettings
-import `in`.testpress.store.network.StoreApiClient
 
 
 class PaymentGatewayFactory {
     fun create(order: Order, activity: Activity): PaymentGateway {
         val instituteSettings: InstituteSettings = TestpressSdk.getTestpressSession(activity)!!.instituteSettings
-        val redirectURL = instituteSettings.baseUrl + StoreApiClient.RAZORPAY_PAYMENT_RESPONSE_PATH
-
         if (instituteSettings.currentPaymentApp.lowercase() == "razorpay")
             return RazorpayPaymentGateway(order, activity)
         return PayuPaymentGateway(order, activity)
