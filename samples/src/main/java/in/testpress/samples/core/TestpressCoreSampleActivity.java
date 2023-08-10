c1e0c1ceebd42aec4e9312eabb82efb0401354cc
PruthiviRaj27 <pruthiviraj@testpress.in>
2023-08-10 10:15:04 +0530

    refactor

--- samples/src/main/java/in/testpress/samples/core/TestpressCoreSampleActivity.java
+++ samples/src/main/java/in/testpress/samples/core/TestpressCoreSampleActivity.java
@@ -119,8 +119,8 @@ public class TestpressCoreSampleActivity extends BaseToolBarActivity {
     }
 
     private void authenticate(String userId, String accessToken, TestpressSdk.Provider provider) {
-        InstituteSettings instituteSettings = new InstituteSettings("https://d0b8-183-82-178-84.ngrok-free.app");
-        instituteSettings.setWhiteLabeledHostUrl("https://d0b8-183-82-178-84.ngrok-free.app");
+        InstituteSettings instituteSettings = new InstituteSettings("https://lmsdemo.testpress.in");
+        instituteSettings.setWhiteLabeledHostUrl("https://lmsdemo.testpress.in");
         TestpressSdk.initialize(this, instituteSettings, userId, accessToken, provider,
                 new TestpressCallback<TestpressSession>() {
                     @Override
