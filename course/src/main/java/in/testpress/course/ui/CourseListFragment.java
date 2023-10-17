59da4075bd06041bd8d3b79989f786e7aea93c35
PruthiviRaj27 <pruthiviraj@testpress.in>
2023-10-17 15:36:12 +0530

    refactor

--- course/src/main/java/in/testpress/course/ui/CourseListFragment.java
+++ course/src/main/java/in/testpress/course/ui/CourseListFragment.java
@@ -10,7 +10,6 @@ import com.google.android.material.tabs.TabLayout;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
