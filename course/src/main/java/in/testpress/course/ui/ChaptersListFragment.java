717bf322c365c49977388cde30e548e08c12dc4e
PruthiviRaj27 <112844543+PruthiviRaj27@users.noreply.github.com>
2024-05-16 11:48:03 +0530

    fix: App crash while opening chapters list fragment via deep link (#652)
    - Added a custom test icon to the ChaptersListFragment in commit 87c8943. We use the `allowCustomTestGeneration` field to handle this icon, which is present in the course object.
    - If the user opens the chapter list fragment via deep link and the `allowCustomTestGeneration` field and course object are null, the app crashes.
    - In this commit, we validate the custom test icon after the course is fetched to prevent the crash.

--- course/src/main/java/in/testpress/course/ui/ChaptersListFragment.java
+++ course/src/main/java/in/testpress/course/ui/ChaptersListFragment.java
@@ -110,7 +110,10 @@ public class ChaptersListFragment extends BaseDataBaseFragment<Chapter, Long> {
         if (getCourse() != null && isItemsEmpty()) {
             showLoadingPlaceholder();
         }
-        setHasOptionsMenu(productSlug == null && isCustomTestGenerationEnabled());
+
+        if (getCourse() != null) {
+            showOrHideCustomTestIconInAppBar();
+        }
     }
 
     private void fetchCourseAndShowChapters(String courseId) {
@@ -123,6 +126,7 @@ public class ChaptersListFragment extends BaseDataBaseFragment<Chapter, Long> {
                         courseDao.insertOrReplace(course);
                         configureList(getActivity(), getListView());
                         refreshWithProgress();
+                        showOrHideCustomTestIconInAppBar();
                     }
 
                     @Override
@@ -130,6 +134,10 @@ public class ChaptersListFragment extends BaseDataBaseFragment<Chapter, Long> {
                         getErrorMessage(exception);
                     }
                 });
+    }
+
+    private void showOrHideCustomTestIconInAppBar(){
+        setHasOptionsMenu(productSlug == null && isCustomTestGenerationEnabled());
     }
 
     private void displayBuyNowButton() {
