51409aa81e035c08f6ecf2b4d6d538e684f445c1
PruthiviRaj27 <pruthiviraj@testpress.in>
2024-06-25 12:16:20 +0530

    refcator

--- exam/src/main/java/in/testpress/exam/ui/TestActivity.java
+++ exam/src/main/java/in/testpress/exam/ui/TestActivity.java
@@ -8,8 +8,6 @@ import android.os.Bundle;
 import androidx.annotation.StringRes;
 import androidx.appcompat.app.AlertDialog;
 import androidx.lifecycle.Observer;
-import androidx.loader.app.LoaderManager;
-import androidx.loader.content.Loader;
 
 import android.view.View;
 import android.widget.Button;
