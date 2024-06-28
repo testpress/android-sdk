792c550cbda97a69d581141619ec7de87a63e38e
PruthiviRaj27 <pruthiviraj@testpress.in>
2024-06-28 18:43:43 +0530

    refcator

---
+++ core/src/main/java/in/testpress/database/entities/OfflineAttemptItem.kt
@@ -0,0 +1,23 @@
+package `in`.testpress.database.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class OfflineAttemptItem(
+    @PrimaryKey(autoGenerate = true)
+    var id: Long = 0,
+    var question: Question,
+    var selectedAnswers: List<Int> = arrayListOf(),
+    var review: Boolean? = null,
+    var savedAnswers: List<Int> = arrayListOf(),
+    var order: Int,
+    var shortText: String? = null,
+    var currentShortText: String? = null,
+    var attemptSection: OfflineAttemptSection? = null,
+    var essayText: String? = null,
+    var localEssayText: String? = null,
+    var files: ArrayList<OfflineUserUploadedFile> = arrayListOf(),
+    var unSyncedFiles: List<String> = arrayListOf(),
+    var attemptId: Long
+)
