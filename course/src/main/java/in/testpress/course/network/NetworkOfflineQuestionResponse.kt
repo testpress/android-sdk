bbaca88715e2b3a56daefc85cdce643e37a80427
PruthiviRaj27 <pruthiviraj@testpress.in>
2024-07-08 10:45:31 +0530

    refcator

--- course/src/main/java/in/testpress/course/network/NetworkOfflineQuestionResponse.kt
+++ course/src/main/java/in/testpress/course/network/NetworkOfflineQuestionResponse.kt
@@ -8,4 +8,56 @@ class NetworkOfflineQuestionResponse(
     val sections: List<Section>,
     val examQuestions: List<ExamQuestion>,
     val questions: List<Question>
-)
+){
+    fun extractUrls(): List<String> {
+        val regexs = listOf(Regex("""src=["'](.*?)["']"""), Regex("""(?:url|src)\((.*?)\)"""))
+        val urls = mutableListOf<String>()
+
+        regexs.forEach { urlPattern ->
+            this.directions.forEach { direction ->
+                direction.html?.let { html ->
+                    urlPattern.findAll(html).forEach { matchResult ->
+                        val url = matchResult.groupValues[1].trim()
+                        urls.add(url)
+                    }
+                }
+            }
+
+            this.sections.forEach { section ->
+                section.instructions?.let { instructions ->
+                    urlPattern.findAll(instructions).forEach { matchResult ->
+                        val url = matchResult.groupValues[1].trim()
+                        urls.add(url)
+                    }
+                }
+            }
+
+            this.questions.forEach { question ->
+                question.questionHtml?.let { questionHtml ->
+                    urlPattern.findAll(questionHtml).forEach { matchResult ->
+                        val url = matchResult.groupValues[1].trim()
+                        urls.add(url)
+                    }
+                }
+                question.translations.forEach { translation ->
+                    translation.questionHtml?.let { translationHtml ->
+                        urlPattern.findAll(translationHtml).forEach { matchResult ->
+                            val url = matchResult.groupValues[1].trim()
+                            urls.add(url)
+                        }
+                    }
+                }
+                question.answers.forEach { answer ->
+                    answer.textHtml?.let { textHtml ->
+                        urlPattern.findAll(textHtml).forEach { matchResult ->
+                            val url = matchResult.groupValues[1].trim()
+                            urls.add(url)
+                        }
+                    }
+                }
+            }
+        }
+
+        return urls
+    }
+}
