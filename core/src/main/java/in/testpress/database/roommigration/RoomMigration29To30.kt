fa9f3c37e5f54e6679cb9b29982afecb0542c24c
PruthiviRaj27 <pruthiviraj@testpress.in>
2025-02-11 12:14:26 +0530

    fix

--- core/src/main/java/in/testpress/database/roommigration/RoomMigration29To30.kt
+++ core/src/main/java/in/testpress/database/roommigration/RoomMigration29To30.kt
@@ -8,6 +8,7 @@ object RoomMigration29To30 {
         override fun migrate(database: SupportSQLiteDatabase) {
             database.execSQL("DROP TABLE IF EXISTS ContentLiteDao")
             database.execSQL("DELETE FROM ContentLiteRemoteKeyDao")
+            database.execSQL("CREATE TABLE IF NOT EXISTS `RunningContentEntity` (`contentOrder` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `id` INTEGER NOT NULL, `type` INTEGER NOT NULL, `order` INTEGER, `chapterId` INTEGER, `freePreview` INTEGER, `title` TEXT, `courseId` INTEGER, `examId` INTEGER, `contentId` INTEGER, `videoId` INTEGER, `attachmentId` INTEGER, `liveStreamId` INTEGER, `contentType` TEXT, `icon` TEXT, `start` TEXT, `end` TEXT, `treePath` TEXT)")
         }
     }
 }
