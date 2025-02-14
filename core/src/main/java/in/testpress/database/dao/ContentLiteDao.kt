10a426e1e9f411523bf02d54d0933aa390740151
PruthiviRaj27 <pruthiviraj@testpress.in>
2025-02-14 16:53:37 +0530

    fix

--- core/src/main/java/in/testpress/database/dao/ContentLiteDao.kt
+++ core/src/main/java/in/testpress/database/dao/ContentLiteDao.kt
@@ -12,6 +12,9 @@ interface ContentLiteDao: BaseDao<ContentEntityLite> {
     @Query("SELECT * FROM runningcontententity WHERE courseId = :courseId AND type = :type")
     fun getRunningContents(courseId: Long, type: Int): PagingSource<Int, ContentEntityLite>
 
+    @Query("SELECT * FROM runningcontententity WHERE courseId = :courseId AND type = :type ORDER BY contentOrder DESC LIMIT 1")
+    suspend fun getLastContent(courseId: Long, type: Int): ContentEntityLite?
+
     @Query("SELECT * FROM runningcontententity WHERE courseId = :courseId AND type = :type")
     fun getUpcomingContents(courseId: Long, type: Int): PagingSource<Int, ContentEntityLite>
 
