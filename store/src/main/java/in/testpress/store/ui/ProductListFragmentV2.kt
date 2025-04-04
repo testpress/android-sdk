2742e393005df82ca937ca0e9ecd3301d3e11a43
PruthiviRaj27 <pruthiviraj@testpress.in>
2025-04-04 11:00:14 +0530

    Refactor ProductListFragmentv2

--- store/src/main/java/in/testpress/store/ui/ProductListFragmentV2.kt
+++ store/src/main/java/in/testpress/store/ui/ProductListFragmentV2.kt
@@ -1,7 +1,6 @@
 package `in`.testpress.store.ui
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,6 +11,7 @@ import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import `in`.testpress.core.TestpressException
+import `in`.testpress.database.entities.ProductLiteEntity
 import `in`.testpress.enums.Status
 import `in`.testpress.fragments.EmptyViewFragment
 import `in`.testpress.fragments.EmptyViewListener
@@ -43,12 +43,12 @@ class ProductListFragmentV2 : Fragment(), EmptyViewListener {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initializeEmptyViewFragment()
+        setupEmptyViewFragment()
         setupRecyclerView()
         observeViewModel()
     }
 
-    private fun initializeEmptyViewFragment() {
+    private fun setupEmptyViewFragment() {
         emptyViewFragment = EmptyViewFragment()
         childFragmentManager.beginTransaction()
             .replace(R.id.empty_view_container, emptyViewFragment)
@@ -58,75 +58,84 @@ class ProductListFragmentV2 : Fragment(), EmptyViewListener {
     private fun setupRecyclerView() {
         adapter = ProductListAdapter(requireContext()) { viewModel.retryNextPage() }
         val layoutManager = LinearLayoutManager(requireContext())
-        binding.productList.addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
-        binding.productList.layoutManager = layoutManager
-        binding.productList.adapter = adapter
+        binding.productList.apply {
+            this.adapter = adapter
+            this.layoutManager = layoutManager
+            this.addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
+        }
+        addPaginationScrollListener(layoutManager)
+    }
 
+    private fun addPaginationScrollListener(layoutManager: LinearLayoutManager) {
         binding.productList.addOnScrollListener(object : RecyclerView.OnScrollListener() {
             override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
                 super.onScrolled(recyclerView, dx, dy)
-                if (dy > 0) { // Check for scroll down
-                    val visibleItemCount = layoutManager.childCount
-                    val totalItemCount = layoutManager.itemCount
-                    val firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition()
-
-                    if ((visibleItemCount + firstVisibleItemPosition) >= totalItemCount && firstVisibleItemPosition >= 0) {
-                        viewModel.fetchNextPage()
-                    }
+                if (dy > 0 && isAtEndOfList(layoutManager)) {
+                    viewModel.fetchNextPage()
                 }
             }
         })
     }
 
+    private fun isAtEndOfList(layoutManager: LinearLayoutManager): Boolean {
+        val visibleItemCount = layoutManager.childCount
+        val totalItemCount = layoutManager.itemCount
+        val firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition()
+        return (visibleItemCount + firstVisibleItemPosition) >= totalItemCount && firstVisibleItemPosition >= 0
+    }
 
     private fun observeViewModel() {
-        viewModel.products.observe(viewLifecycleOwner) { resource->
+        viewModel.products.observe(viewLifecycleOwner) { resource ->
             when (resource?.status) {
-                Status.LOADING -> {
-                    binding.emptyViewContainer.isVisible = false
-                    if (adapter.itemCount > 0){
-                        adapter.updateFooterState(FooterState.LOADING)
-                    } else {
-                        binding.shimmerViewContainer.isVisible = true
-                    }
-                }
-                Status.SUCCESS -> {
-                    adapter.submitList(resource.data)
-                    adapter.updateFooterState(FooterState.HIDDEN)
-                    if (adapter.itemCount == 0){
-                        binding.emptyViewContainer.isVisible = true
-                        showEmptyScreen()
-                    } else {
-                        binding.productList.isVisible = true
-                        binding.emptyViewContainer.isVisible = false
-                    }
-                    binding.shimmerViewContainer.isVisible = false
-                }
-                Status.ERROR -> {
-                    binding.shimmerViewContainer.isVisible = false
-                    if (adapter.itemCount > 0) {
-                        binding.productList.isVisible = true
-                        adapter.updateFooterState(FooterState.ERROR)
-                    } else {
-                        binding.productList.isVisible = false
-                        binding.emptyViewContainer.isVisible = true
-                        emptyViewFragment.displayError(resource.exception!!)
-                        resource.exception?.let {
-                            emptyViewFragment.displayError(it)
-                        } ?: emptyViewFragment.displayError(
-                            TestpressException.unexpectedWebViewError(UnknownError())
-                        )
-                    }
-                }
-                else -> {}
+                Status.LOADING -> handleLoadingState()
+                Status.SUCCESS -> handleSuccessState(resource.data)
+                Status.ERROR -> handleErrorState(resource.exception)
+                else -> { /* No-op */ }
             }
         }
     }
 
-    private fun showEmptyScreen() {
-        emptyViewFragment.setEmptyText(R.string.no_course_title, R.string.no_course_description, null)
-        emptyViewFragment.setImage(R.drawable.empty_cart)
-        emptyViewFragment.showOrHideButton(false)
+    private fun handleLoadingState() {
+        binding.emptyViewContainer.isVisible = false
+        if (adapter.itemCount > 0) {
+            adapter.updateFooterState(FooterState.LOADING)
+        } else {
+            binding.shimmerViewContainer.isVisible = true
+        }
+    }
+
+    private fun handleSuccessState(data: List<ProductLiteEntity>?) {
+        adapter.submitList(data)
+        adapter.updateFooterState(FooterState.HIDDEN)
+        if (adapter.itemCount == 0) {
+            binding.emptyViewContainer.isVisible = true
+            displayEmptyScreen()
+        } else {
+            binding.productList.isVisible = true
+            binding.emptyViewContainer.isVisible = false
+        }
+        binding.shimmerViewContainer.isVisible = false
+    }
+
+    private fun handleErrorState(exception: TestpressException?) {
+        binding.shimmerViewContainer.isVisible = false
+        if (adapter.itemCount > 0) {
+            binding.productList.isVisible = true
+            adapter.updateFooterState(FooterState.ERROR)
+        } else {
+            binding.productList.isVisible = false
+            binding.emptyViewContainer.isVisible = true
+            val errorToDisplay = exception ?: TestpressException.unexpectedWebViewError(UnknownError())
+            emptyViewFragment.displayError(errorToDisplay)
+        }
+    }
+
+    private fun displayEmptyScreen() {
+        emptyViewFragment.apply {
+            setEmptyText(R.string.no_course_title, R.string.no_course_description, null)
+            setImage(R.drawable.empty_cart)
+            showOrHideButton(false)
+        }
     }
 
     override fun onDestroyView() {
