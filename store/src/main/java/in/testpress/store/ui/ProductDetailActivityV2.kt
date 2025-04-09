32a8441f718f6b22d5c435356212ff549d1510ac
PruthiviRaj27 <pruthiviraj@testpress.in>
2025-04-09 18:08:47 +0530

    fix

--- store/src/main/java/in/testpress/store/ui/ProductDetailActivityV2.kt
+++ store/src/main/java/in/testpress/store/ui/ProductDetailActivityV2.kt
@@ -1,17 +1,17 @@
 package `in`.testpress.store.ui
 
 import android.os.Bundle
-import `in`.testpress.store.databinding.TestpressProductDetailsLayoutBinding
+import `in`.testpress.store.databinding.TestpressProductDetailsV2LayoutBinding
 import `in`.testpress.ui.BaseToolBarActivity
 
 class ProductDetailsActivityV2 : BaseToolBarActivity() {
 
-    private lateinit var binding: TestpressProductDetailsLayoutBinding
+    private lateinit var binding: TestpressProductDetailsV2LayoutBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = TestpressProductDetailsLayoutBinding.inflate(layoutInflater)
+        binding = TestpressProductDetailsV2LayoutBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
     }
