c7e6535a472c06381e669ea3c67ea654e267f27a
PruthiviRaj27 <pruthiviraj@testpress.in>
2025-04-09 11:53:23 +0530

    fix

--- store/src/main/java/in/testpress/store/data/repository/ProductListRepository.kt
+++ store/src/main/java/in/testpress/store/data/repository/ProductListRepository.kt
@@ -12,7 +12,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 
-class ProductRepository(
+class ProductListRepository(
     context: Context,
     scope: CoroutineScope = CoroutineScope(Dispatchers.IO + SupervisorJob())
 ) :
