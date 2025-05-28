df765e1e8faa185ae2112f42838d8a256850179f
PruthiviRaj <112844543+PruthiviRaj27@users.noreply.github.com>
2025-05-28 14:59:36 +0530

    feat: Add OfflineAttachment entity and migration to database schema version 36 (#870)
    <!-- This is an auto-generated comment: release notes by coderabbit.ai -->
    
    ## Summary by CodeRabbit
    
    - **New Features**
    - Introduced support for offline downloadable attachments, allowing users to track the status and progress of attachment downloads within the app.
    
    - **Chores**
    - Updated the app’s database to a new version to support offline attachment tracking.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

---
+++ core/src/main/java/in/testpress/database/entities/OfflineAttachment.kt
@@ -0,0 +1,18 @@
+package `in`.testpress.database.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class OfflineAttachment(
+    @PrimaryKey val id: Long,
+    val title: String,
+    val url: String,
+    val path: String,
+    val status: OfflineAttachmentDownloadStatus,
+    val progress: Int = 0
+)
+
+enum class OfflineAttachmentDownloadStatus {
+    QUEUED, DOWNLOADING, FAILED, DOWNLOADED
+}
