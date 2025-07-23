4e39706a1262746fe8e2ca0aa43e3f6d0baaba84
pruthiviraj <pruthiviraj@testpress.in>
2025-07-23 15:42:21 +0530

    fix

---
+++ exam/src/main/java/in/testpress/exam/ui/SearchActivity.kt
@@ -0,0 +1,17 @@
+package `in`.testpress.exam.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import `in`.testpress.exam.R
+
+class SearchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.testpress_container_layout_without_toolbar)
+        val searchFragment = SearchFragment()
+        searchFragment.arguments = intent.extras
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, searchFragment).commitAllowingStateLoss()
+    }
+}
