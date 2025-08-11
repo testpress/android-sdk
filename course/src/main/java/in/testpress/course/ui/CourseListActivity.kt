b062ad9ed58df59f8f158d3b7be4bb312503bce3
pruthiviraj <pruthiviraj@testpress.in>
2025-08-11 11:17:39 +0530

    fix

--- course/src/main/java/in/testpress/course/ui/CourseListActivity.kt
+++ course/src/main/java/in/testpress/course/ui/CourseListActivity.kt
@@ -20,14 +20,13 @@ class CourseListActivity : BaseToolBarActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (
-            requestCode == TestpressStore.STORE_REQUEST_CODE &&
-            resultCode == Activity.RESULT_OK &&
-            data != null &&
-            !data.getBooleanExtra(TestpressStore.CONTINUE_PURCHASE, false)
-        ) {
-            supportFragmentManager.fragments.forEach { fragment ->
-                fragment.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == TestpressStore.STORE_REQUEST_CODE && resultCode == Activity.RESULT_OK){
+            data?.let {  intentData ->
+                if (!intentData.getBooleanExtra(TestpressStore.CONTINUE_PURCHASE, false)) {
+                    supportFragmentManager.fragments.forEach { fragment ->
+                        fragment.onActivityResult(requestCode, resultCode, intentData)
+                    }
+                }
             }
         }
     }
