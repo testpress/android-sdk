06033a53840a1612901b63352a88fbab6b1ec1a8
Gowreesh-A-M <gowreesh@testpress.in>
2025-08-25 14:33:54 +0530

    refactor: migrate ImageUtils from Java to Kotlin

--- core/src/main/java/in/testpress/util/ImageUtils.kt
+++ core/src/main/java/in/testpress/util/ImageUtils.kt
@@ -1,17 +1,16 @@
-package in.testpress.util;
+package `in`.testpress.util
 
-import android.content.Context;
+import android.content.Context
+import com.nostra13.universalimageloader.cache.memory.impl.WeakMemoryCache
+import com.nostra13.universalimageloader.core.DisplayImageOptions
+import com.nostra13.universalimageloader.core.ImageLoader
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration
+import com.nostra13.universalimageloader.core.assist.ImageScaleType
+import com.nostra13.universalimageloader.core.display.FadeInBitmapDisplayer
 
-import com.nostra13.universalimageloader.cache.memory.impl.WeakMemoryCache;
-import com.nostra13.universalimageloader.core.DisplayImageOptions;
-import com.nostra13.universalimageloader.core.ImageLoader;
-import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
-import com.nostra13.universalimageloader.core.assist.ImageScaleType;
-import com.nostra13.universalimageloader.core.display.FadeInBitmapDisplayer;
+import `in`.testpress.R
 
-import in.testpress.R;
-
-public class ImageUtils {
+object ImageUtils {
 
     /**
      * Init the ImageLoader with custom configuration.
@@ -19,44 +18,45 @@ public class ImageUtils {
      * @param context Context
      * @return Initialized ImageLoader.
      */
-    public static ImageLoader initImageLoader(Context context) {
-        if (context == null) {
-            throw new IllegalArgumentException("Context must not be null.");
-        }
-        ImageLoader imageLoader = ImageLoader.getInstance();
-        if (imageLoader.isInited()) {
-            return imageLoader;
+    @JvmStatic
+    fun initImageLoader(context: Context?): ImageLoader {
+        requireNotNull(context) { "Context must not be null." }
+
+        val imageLoader = ImageLoader.getInstance()
+        return if (imageLoader.isInited) {
+            imageLoader
         } else {
-            DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder()
-                    .cacheOnDisk(true).cacheInMemory(true)
-                    .imageScaleType(ImageScaleType.EXACTLY)
-                    .displayer(new FadeInBitmapDisplayer(300)).build();
+            val defaultOptions = DisplayImageOptions.Builder()
+                .cacheOnDisk(true).cacheInMemory(true)
+                .imageScaleType(ImageScaleType.EXACTLY)
+                .displayer(FadeInBitmapDisplayer(300)).build()
 
-            ImageLoaderConfiguration config =
-                    new ImageLoaderConfiguration.Builder(context.getApplicationContext())
-                            .defaultDisplayImageOptions(defaultOptions)
-                            .memoryCache(new WeakMemoryCache())
-                            .diskCacheSize(500 * 1024 * 1024).build();
+            val config = ImageLoaderConfiguration.Builder(context.applicationContext)
+                .defaultDisplayImageOptions(defaultOptions)
+                .memoryCache(WeakMemoryCache())
+                .diskCacheSize(500 * 1024 * 1024).build()
 
-            imageLoader.init(config);
-            return imageLoader;
+            imageLoader.init(config)
+            imageLoader
         }
     }
 
-    public static DisplayImageOptions getPlaceholdersOption() {
-        return new DisplayImageOptions.Builder().cacheInMemory(true)
-                .cacheOnDisk(true).resetViewBeforeLoading(true)
-                .imageScaleType(ImageScaleType.EXACTLY)
-                .showImageForEmptyUri(R.drawable.testpress_placeholder_icon)
-                .showImageOnFail(R.drawable.testpress_placeholder_icon)
-                .showImageOnLoading(R.drawable.testpress_placeholder_icon).build();
+    @JvmStatic
+    fun getPlaceholdersOption(): DisplayImageOptions {
+        return DisplayImageOptions.Builder().cacheInMemory(true)
+            .cacheOnDisk(true).resetViewBeforeLoading(true)
+            .imageScaleType(ImageScaleType.EXACTLY)
+            .showImageForEmptyUri(R.drawable.testpress_placeholder_icon)
+            .showImageOnFail(R.drawable.testpress_placeholder_icon)
+            .showImageOnLoading(R.drawable.testpress_placeholder_icon).build()
     }
 
-    public static DisplayImageOptions getAvatarPlaceholdersOption() {
-        return new DisplayImageOptions.Builder().cacheInMemory(true)
-                .cacheOnDisk(true).resetViewBeforeLoading(true)
-                .showImageForEmptyUri(R.drawable.profile_image_place_holder)
-                .showImageOnFail(R.drawable.profile_image_place_holder)
-                .showImageOnLoading(R.drawable.profile_image_place_holder).build();
+    @JvmStatic
+    fun getAvatarPlaceholdersOption(): DisplayImageOptions {
+        return DisplayImageOptions.Builder().cacheInMemory(true)
+            .cacheOnDisk(true).resetViewBeforeLoading(true)
+            .showImageForEmptyUri(R.drawable.profile_image_place_holder)
+            .showImageOnFail(R.drawable.profile_image_place_holder)
+            .showImageOnLoading(R.drawable.profile_image_place_holder).build()
     }
 }
