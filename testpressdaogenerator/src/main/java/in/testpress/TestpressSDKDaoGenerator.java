556ab833346d82c59a280b3025d866b4036f4e4d
Harish V <harishv@testpress.in>
2025-10-31 20:08:43 +0530

    refactor: Update schema version and remove unused learnlens_asset_id field (#983)
    - Incremented schema version from 75 to 77 in DaoMaster and
    TestpressSDKDaoGenerator.
    - Removed the learnlens_asset_id field from the Content model as it is
    no longer utilized.

--- testpressdaogenerator/src/main/java/in/testpress/TestpressSDKDaoGenerator.java
+++ testpressdaogenerator/src/main/java/in/testpress/TestpressSDKDaoGenerator.java
@@ -10,7 +10,7 @@ import org.greenrobot.greendao.generator.ToOne;
 
 public class TestpressSDKDaoGenerator {
     // Increase the version if any modification has been made in this file.
-    private static final int VERSION = 76;
+    private static final int VERSION = 77;
 
     public static void main(String args[]) throws Exception {
         Schema schema = new Schema(VERSION, "in.testpress.models.greendao");
@@ -380,9 +380,7 @@ public class TestpressSDKDaoGenerator {
         content.addBooleanProperty("isAIEnabled")
                 .columnName("IS_AI_ENABLED")
                 .codeBeforeField("@SerializedName(\"is_ai_enabled\")");
-        content.addStringProperty("learnlensAssetId")
-                .columnName("LEARNLENS_ASSET_ID")
-                .codeBeforeField("@SerializedName(\"learnlens_asset_id\")");
+        content.addStringProperty("learnlensAssetId");
         content.implementsInterface("android.os.Parcelable");
         return content;
     }
