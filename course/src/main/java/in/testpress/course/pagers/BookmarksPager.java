cdbd99c00b3ae396746f87821f26cdf1cc466d95
Gowreesh-A-M <gowreesh@testpress.in>
2025-12-05 17:27:41 +0530

    chore: Add stream collector import

--- course/src/main/java/in/testpress/course/pagers/BookmarksPager.java
+++ course/src/main/java/in/testpress/course/pagers/BookmarksPager.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import in.testpress.core.TestpressSDKDatabase;
 import in.testpress.exam.api.TestpressExamApiClient;
